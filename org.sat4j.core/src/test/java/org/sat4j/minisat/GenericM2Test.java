0cf3b07724494c1d4376754d0dbe4ef8f211c692
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2008-07-15 20:54:04 +0000

    Wrote the test another way. Works now :)
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@93 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

--- org.sat4j.core/src/test/java/org/sat4j/minisat/GenericM2Test.java
+++ org.sat4j.core/src/test/java/org/sat4j/minisat/GenericM2Test.java
@@ -6,57 +6,54 @@
  */
 package org.sat4j.minisat;
 
-import java.util.Iterator;
-
 import junit.framework.TestSuite;
 
-import org.sat4j.core.Vec;
 import org.sat4j.specs.ISolver;
 
 /**
  * @author leberre
  * 
- * To change the template for this generated type comment go to Window -
- * Preferences - Java - Code Generation - Code and Comments
+ *         To change the template for this generated type comment go to Window -
+ *         Preferences - Java - Code Generation - Code and Comments
  */
 public class GenericM2Test extends AbstractM2Test<ISolver> {
 
-    private String solvername;
+	private String solvername;
 
-    private static final SolverFactory factory = SolverFactory.instance();
+	private static final SolverFactory factory = SolverFactory.instance();
 
-    /**
-     * @param arg0
-     */
-    public GenericM2Test(String arg0) {
-        setName("AbstractM2Test" + arg0);
-        solvername = arg0;
-    }
+	/**
+	 * @param arg0
+	 */
+	public GenericM2Test(String arg0) {
+		setName("AbstractM2Test" + arg0);
+		solvername = arg0;
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see junit.framework.TestCase#setUp()
-     */
-    @Override
-    protected ISolver createSolver() {
-        return factory.createSolverByName(solvername);
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	@Override
+	protected ISolver createSolver() {
+		return factory.createSolverByName(solvername);
+	}
 
-    public static TestSuite suite() {
-        TestSuite suite = new TestSuite();
-        String[] names = factory.solverNames();
-        Vec<String> vnames = new Vec<String>(names);
-        vnames.remove("Mini3SAT");
-        vnames.remove("DimacsOutput");
-        for (Iterator<String> it = vnames.iterator() ; it.hasNext();) {
-            suite.addTest(new GenericM2Test(it.next()));
-        }
-        return suite;
-    }
+	public static TestSuite suite() {
+		TestSuite suite = new TestSuite();
+		String[] names = factory.solverNames();
+		String name;
+		for (int i = 0; i < names.length; i++) {
+			name = names[i];
+			if (!"Mini3SAT".equals(name) && !"DimacsOutput".equals(name))
+				suite.addTest(new GenericM2Test(name));
+		}
+		return suite;
+	}
 
-    @Override
-    protected void runTest() throws Throwable {
-        assertFalse(solveInstance(PREFIX + "pigeons/hole6.cnf"));
-    }
+	@Override
+	protected void runTest() throws Throwable {
+		assertFalse(solveInstance(PREFIX + "pigeons/hole6.cnf"));
+	}
 }
