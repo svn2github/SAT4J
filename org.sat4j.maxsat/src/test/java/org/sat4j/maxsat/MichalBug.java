89d7b26d3d8e38c5f7e639b13cae7d057a2dd326
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2008-09-01 21:16:12 +0000

    Added example of OptToSatAdapter to Michal example.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@131 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

--- org.sat4j.maxsat/src/test/java/org/sat4j/maxsat/MichalBug.java
+++ org.sat4j.maxsat/src/test/java/org/sat4j/maxsat/MichalBug.java
@@ -6,7 +6,9 @@ import org.junit.Test;
 import org.sat4j.core.VecInt;
 import org.sat4j.specs.ContradictionException;
 import org.sat4j.specs.IOptimizationProblem;
+import org.sat4j.specs.IProblem;
 import org.sat4j.specs.TimeoutException;
+import org.sat4j.tools.OptToSatAdapter;
 
 public class MichalBug {
 
@@ -53,7 +55,39 @@ public class MichalBug {
 			exitCode = OPTIMUM_FOUND;
 		}
 
-		assertEquals(exitCode, OPTIMUM_FOUND);
+		assertEquals(OPTIMUM_FOUND,exitCode);
+		int[] model = problem.model();
+		assertEquals(2,model.length);
+		assertEquals(-1,model[0]);
+		assertEquals(-2,model[1]);
+	}
+	
+	@Test
+	public void testMichalWithOptAdapter() throws ContradictionException,
+			TimeoutException {
+		WeightedMaxSatDecorator maxSATSolver = new WeightedMaxSatDecorator(
+				SolverFactory.newLight());
+
+		maxSATSolver.newVar(2);
+		maxSATSolver.setExpectedNumberOfClauses(4);
+
+		int[] clause_1 = { 1, 1, 2 };
+		maxSATSolver.addClause(new VecInt(clause_1));
+
+		int[] clause_2 = { 100, -1, -2 };
+		maxSATSolver.addClause(new VecInt(clause_2));
+
+		int[] clause_3 = { 1000, 1, -2 };
+		maxSATSolver.addClause(new VecInt(clause_3));
+
+		int[] clause_4 = { 100000, -1, 2 };
+		maxSATSolver.addClause(new VecInt(clause_4));
+
+		IProblem problem = new OptToSatAdapter(maxSATSolver);
+
+		boolean isSatisfiable = problem.isSatisfiable();
+
+		assertEquals(true, isSatisfiable);
 		int[] model = problem.model();
 		assertEquals(2,model.length);
 		assertEquals(-1,model[0]);
