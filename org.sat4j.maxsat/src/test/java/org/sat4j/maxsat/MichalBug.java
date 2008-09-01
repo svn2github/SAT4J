e3dbe55680ecdf14e6ad85c30fc26bf497a2e8e7
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2008-09-01 19:37:59 +0000

    Added Michal problematic scenario: http://forge.objectweb.org/forum/message.php?msg_id=12622
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@126 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

---
+++ org.sat4j.maxsat/src/test/java/org/sat4j/maxsat/MichalBug.java
@@ -0,0 +1,63 @@
+package org.sat4j.maxsat;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.sat4j.core.VecInt;
+import org.sat4j.specs.ContradictionException;
+import org.sat4j.specs.IOptimizationProblem;
+import org.sat4j.specs.TimeoutException;
+
+public class MichalBug {
+
+	@Test
+	public void testMichalReportedProblem() throws ContradictionException,
+			TimeoutException {
+		WeightedMaxSatDecorator maxSATSolver = new WeightedMaxSatDecorator(
+				SolverFactory.newLight());
+
+		final int OPTIMUM_FOUND = 0;
+		final int UNSATISFIABLE = 1;
+
+		maxSATSolver.newVar(2);
+		maxSATSolver.setExpectedNumberOfClauses(4);
+
+		int[] clause_1 = { 1, 1, 2 };
+		maxSATSolver.addClause(new VecInt(clause_1));
+
+		int[] clause_2 = { 100, -1, -2 };
+		maxSATSolver.addClause(new VecInt(clause_2));
+
+		int[] clause_3 = { 1000, 1, -2 };
+		maxSATSolver.addClause(new VecInt(clause_3));
+
+		int[] clause_4 = { 100000, -1, 2 };
+		maxSATSolver.addClause(new VecInt(clause_4));
+
+		IOptimizationProblem problem = maxSATSolver;
+
+		int exitCode = UNSATISFIABLE;
+		boolean isSatisfiable = false;
+		try {
+			while (problem.admitABetterSolution()) {
+				isSatisfiable = true;
+				problem.calculateObjective();
+				problem.discard();
+			}
+			if (isSatisfiable) {
+				exitCode = OPTIMUM_FOUND;
+			} else {
+				exitCode = UNSATISFIABLE;
+			}
+		} catch (ContradictionException ex) {
+			assert (isSatisfiable);
+			exitCode = OPTIMUM_FOUND;
+		}
+
+		assertEquals(exitCode, OPTIMUM_FOUND);
+		int[] model = problem.model();
+		assertEquals(2,model.length);
+		assertEquals(-1,model[0]);
+		assertEquals(-2,model[1]);
+	}
+}
