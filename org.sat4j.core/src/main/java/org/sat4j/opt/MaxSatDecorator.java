9669cc785444fbf98145179be75e822d644c5c8a
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2008-09-17 13:07:08 +0000

    Fixed infinite recursive call found by Findbugs 1.3.5.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@135 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

--- org.sat4j.core/src/main/java/org/sat4j/opt/MaxSatDecorator.java
+++ org.sat4j.core/src/main/java/org/sat4j/opt/MaxSatDecorator.java
@@ -83,7 +83,7 @@ public class MaxSatDecorator extends AbstractSelectorVariablesDecorator {
     }
 
     public Number calculateObjective() {
-        calculateObjective();
+        calculateObjectiveValue();
         return counter;
     }
 
