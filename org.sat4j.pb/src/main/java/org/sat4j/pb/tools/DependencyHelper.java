31c6ad938e5fe75607daacc437fd272f86ea3c8a
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2009-01-08 20:36:54 +0000

    Added a new class to allow an easy way to set the objective function.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@260 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

--- org.sat4j.pb/src/main/java/org/sat4j/pb/tools/DependencyHelper.java
+++ org.sat4j.pb/src/main/java/org/sat4j/pb/tools/DependencyHelper.java
@@ -1,5 +1,25 @@
+/*******************************************************************************
+* SAT4J: a SATisfiability library for Java Copyright (C) 2004-2008 Daniel Le Berre
+*
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Alternatively, the contents of this file may be used under the terms of
+* either the GNU Lesser General Public License Version 2.1 or later (the
+* "LGPL"), in which case the provisions of the LGPL are applicable instead
+* of those above. If you wish to allow use of your version of this file only
+* under the terms of the LGPL, and not to allow others to use your version of
+* this file under the terms of the EPL, indicate your decision by deleting
+* the provisions above and replace them with the notice and other provisions
+* required by the LGPL. If you do not delete the provisions above, a recipient
+* may use your version of this file under the terms of the EPL or the LGPL.
+*******************************************************************************/
+
 package org.sat4j.pb.tools;
 
+import java.math.BigInteger;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -9,8 +29,8 @@ import java.util.TreeSet;
 import org.sat4j.core.LiteralsUtils;
 import org.sat4j.core.Vec;
 import org.sat4j.core.VecInt;
-import org.sat4j.minisat.core.Solver;
 import org.sat4j.pb.IPBSolver;
+import org.sat4j.pb.ObjectiveFunction;
 import org.sat4j.specs.ContradictionException;
 import org.sat4j.specs.IConstr;
 import org.sat4j.specs.IVec;
@@ -43,7 +63,7 @@ public class DependencyHelper<T,C> implements SearchListener {
 	private int conflictingVariable;
 
 	final XplainPB xplain;
-
+	
 	public DependencyHelper(IPBSolver solver, int maxvarid) {
 		this.xplain = new XplainPB(solver);
 		xplain.newVar(maxvarid);
@@ -147,6 +167,17 @@ public class DependencyHelper<T,C> implements SearchListener {
 		return new ImplicationNamer<T,C>(this, toName);
 	}
 
+	public void setObectiveFunction(WeightedObject<T> ... wobj) {
+		IVecInt literals = new VecInt(wobj.length);
+		IVec<BigInteger> coefs = new Vec<BigInteger>(wobj.length);
+		for (WeightedObject<T> wo : wobj) {
+			literals.push(getIntValue(wo.thing));
+			coefs.push(wo.getWeight());
+		}
+		ObjectiveFunction obj = new ObjectiveFunction(literals,coefs);
+		xplain.setObjectiveFunction(obj);
+	}
+	
 	public void adding(int p) {
 		// TODO Auto-generated method stub
 
--- org.sat4j.pb/src/test/java/org/sat4j/pb/TestDependencyHelper.java
+++ org.sat4j.pb/src/test/java/org/sat4j/pb/TestDependencyHelper.java
@@ -1,8 +1,28 @@
+/*******************************************************************************
+* SAT4J: a SATisfiability library for Java Copyright (C) 2004-2008 Daniel Le Berre
+*
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Alternatively, the contents of this file may be used under the terms of
+* either the GNU Lesser General Public License Version 2.1 or later (the
+* "LGPL"), in which case the provisions of the LGPL are applicable instead
+* of those above. If you wish to allow use of your version of this file only
+* under the terms of the LGPL, and not to allow others to use your version of
+* this file under the terms of the EPL, indicate your decision by deleting
+* the provisions above and replace them with the notice and other provisions
+* required by the LGPL. If you do not delete the provisions above, a recipient
+* may use your version of this file under the terms of the EPL or the LGPL.
+*******************************************************************************/
+
 package org.sat4j.pb;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.sat4j.pb.tools.WeightedObject.newWO;
 
 import java.util.Iterator;
 import java.util.Set;
@@ -123,4 +143,21 @@ public class TestDependencyHelper {
 		assertEquals("User selection",it.next());
 	}
 	
+	@Test
+	public void testObjectiveFunction() throws ContradictionException, TimeoutException {
+		helper.implication("A").implies("B").and("C").and("D").named("I1");
+		helper.implication("C").implies("C1","C2","C3").named("C versions");
+		helper.atMost(1,"C1","C2","C3").named("Singleton on C");
+		helper.setTrue("A","User selection");
+		helper.setObectiveFunction(newWO("C1",4),newWO("C2",2),newWO("C3",1));
+		assertTrue(helper.hasASolution());
+		IVec<String> solution = helper.getSolution();
+		assertTrue(solution.contains("A"));
+		assertTrue(solution.contains("B"));
+		assertTrue(solution.contains("C"));
+		assertTrue(solution.contains("C3"));
+		assertFalse(solution.contains("C2"));
+		assertFalse(solution.contains("C1"));
+		assertTrue(solution.contains("D"));
+	}
 }
