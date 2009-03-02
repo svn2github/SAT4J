8ac4569bb2c7633f94ebf2fcaa45df8db25ee5e0
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2009-03-02 19:11:58 +0000

    Added also Pair code else it breaks the code on HEAD.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@346 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

--- org.sat4j.core/src/main/java/org/sat4j/tools/xplain/Pair.java
+++ org.sat4j.core/src/main/java/org/sat4j/tools/xplain/Pair.java
@@ -1,16 +1,36 @@
 package org.sat4j.tools.xplain;
 
+import org.sat4j.specs.IConstr;
+
 public class Pair implements Comparable<Pair> {
 	public final Integer key;
-	public final double activity;
+	public final IConstr constr;
+	public static final double NOTHING = 0.0001;
 
-	public Pair(Integer key, double activity) {
+	public Pair(Integer key, IConstr constr) {
 		this.key = key;
-		this.activity = activity;
+		this.constr = constr;
 	}
 
 	public int compareTo(Pair arg0) {
-		return activity > arg0.activity ? -1 : 1;
+		if (arg0.constr == constr) {
+			return 0;
+		}
+		if (arg0.constr == null) {
+			return -1;
+		}
+		if (constr == null) {
+			return 1;
+		}
+		double deltaActivity = constr.getActivity() - arg0.constr.getActivity();
+		if (Math.abs(deltaActivity) < NOTHING) {
+			int deltaSize = constr.size() - arg0.constr.size();
+			if (deltaSize == 0) {
+				return key - arg0.key;
+			}
+			return deltaSize;
+		}
+		return deltaActivity > 0 ? -1 : 1;
 	}
 
 }
