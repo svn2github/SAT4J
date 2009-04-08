159303f8e0dc3125a1cdd14018c908670dd1efa2
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2009-04-08 10:09:58 +0000

    Using WL version of PB data structure by default.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@378 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

--- org.sat4j.pb/src/main/java/org/sat4j/pb/SolverFactory.java
+++ org.sat4j.pb/src/main/java/org/sat4j/pb/SolverFactory.java
@@ -461,7 +461,7 @@ public class SolverFactory extends ASolverFactory<IPBSolver> {
 	 *      instance of ASolverFactory.
 	 */
 	public static IPBSolver newDefault() {
-		return newCompetPBResHTMixedConstraintsObjectiveExpSimp();
+		return newCompetPBResMinHTMixedConstraintsObjectiveExpSimp();
 	}
 
 	@Override
