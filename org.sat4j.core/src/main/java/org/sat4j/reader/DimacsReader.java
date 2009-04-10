5a27b4411c9d8fa0a4df72c1513f90ba20cfed8b
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2009-04-10 15:13:15 +0000

    Fixed problem of multi spaces between numbers in parameter line.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@401 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

--- org.sat4j.core/src/main/java/org/sat4j/reader/DimacsReader.java
+++ org.sat4j.core/src/main/java/org/sat4j/reader/DimacsReader.java
@@ -114,7 +114,7 @@ public class DimacsReader extends Reader implements Serializable {
 			throw new ParseFormatException(
 					"premature end of file: <p cnf ...> expected");
 		}
-		String[] tokens = line.split(" ");
+		String[] tokens = line.split("\\s+");
 		if (tokens.length < 4 || !"p".equals(tokens[0])
 				|| !formatString.equals(tokens[1])) {
 			throw new ParseFormatException("problem line expected (p cnf ...)");
