c512e52f36b3e3b9005f62a8e7c1c0a241ad3bbc
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2009-05-15 18:21:10 +0000

    Fixed ECJ warning.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@445 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

--- org.sat4j.pb/src/main/java/org/sat4j/pb/reader/OPBReader2007.java
+++ org.sat4j.pb/src/main/java/org/sat4j/pb/reader/OPBReader2007.java
@@ -1,30 +1,30 @@
 /*******************************************************************************
-* SAT4J: a SATisfiability library for Java Copyright (C) 2004-2008 Daniel Le Berre
-*
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Alternatively, the contents of this file may be used under the terms of
-* either the GNU Lesser General Public License Version 2.1 or later (the
-* "LGPL"), in which case the provisions of the LGPL are applicable instead
-* of those above. If you wish to allow use of your version of this file only
-* under the terms of the LGPL, and not to allow others to use your version of
-* this file under the terms of the EPL, indicate your decision by deleting
-* the provisions above and replace them with the notice and other provisions
-* required by the LGPL. If you do not delete the provisions above, a recipient
-* may use your version of this file under the terms of the EPL or the LGPL.
-* 
-* Based on the pseudo boolean algorithms described in:
-* A fast pseudo-Boolean constraint solver Chai, D.; Kuehlmann, A.
-* Computer-Aided Design of Integrated Circuits and Systems, IEEE Transactions on
-* Volume 24, Issue 3, March 2005 Page(s): 305 - 317
-* 
-* and 
-* Heidi E. Dixon, 2004. Automating Pseudo-Boolean Inference within a DPLL 
-* Framework. Ph.D. Dissertation, University of Oregon.
-*******************************************************************************/
+ * SAT4J: a SATisfiability library for Java Copyright (C) 2004-2008 Daniel Le Berre
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU Lesser General Public License Version 2.1 or later (the
+ * "LGPL"), in which case the provisions of the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of the LGPL, and not to allow others to use your version of
+ * this file under the terms of the EPL, indicate your decision by deleting
+ * the provisions above and replace them with the notice and other provisions
+ * required by the LGPL. If you do not delete the provisions above, a recipient
+ * may use your version of this file under the terms of the EPL or the LGPL.
+ * 
+ * Based on the pseudo boolean algorithms described in:
+ * A fast pseudo-Boolean constraint solver Chai, D.; Kuehlmann, A.
+ * Computer-Aided Design of Integrated Circuits and Systems, IEEE Transactions on
+ * Volume 24, Issue 3, March 2005 Page(s): 305 - 317
+ * 
+ * and 
+ * Heidi E. Dixon, 2004. Automating Pseudo-Boolean Inference within a DPLL 
+ * Framework. Ph.D. Dissertation, University of Oregon.
+ *******************************************************************************/
 /*=============================================================================
  * parser for CSP instances represented in XML format
  * 
@@ -68,9 +68,8 @@ import org.sat4j.specs.IVecInt;
 /**
  * Reader complying with the PB07 input format.
  * 
- * Non-linear to linear translation adapted from the PB07 readers provided 
- * by Olivier Roussel and Vasco Manquinho
- * (was available in C++, not in Java)
+ * Non-linear to linear translation adapted from the PB07 readers provided by
+ * Olivier Roussel and Vasco Manquinho (was available in C++, not in Java)
  * 
  * http://www.cril.univ-artois.fr/PB07/parser/SimpleParser.java
  * http://www.cril.univ-artois.fr/PB07/parser/SimpleParser.cc
@@ -81,207 +80,203 @@ import org.sat4j.specs.IVecInt;
  */
 public class OPBReader2007 extends OPBReader2006 {
 
-    /**
+	/**
      * 
      */
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    private int nbProducts;
+	/**
+	 * @param solver
+	 */
+	public OPBReader2007(IPBSolver solver) {
+		super(solver);
+	}
 
-    // MetaData: #products and productSize in file.
+	@Override
+	protected boolean isGoodFirstCharacter(char c) {
+		return Character.isLetter(c) || c == '_' || c == '~';
+	}
 
-    /**
-     * @param solver
-     */
-    public OPBReader2007(IPBSolver solver) {
-        super(solver);
-    }
+	@Override
+	protected void checkId(StringBuffer s) throws ParseFormatException {
+		// Small check on the coefficient ID to make sure everything is ok
+		int cpt = 1;
+		if (s.charAt(0) == '~')
+			cpt = 2;
+		int varID = Integer.parseInt(s.substring(cpt));
+		if (varID > nbVars) {
+			throw new ParseFormatException(
+					"Variable identifier larger than #variables in metadata.");
+		}
+	}
 
-    @Override
-    protected boolean isGoodFirstCharacter(char c) {
-        return Character.isLetter(c) || c == '_' || c == '~';
-    }
+	/**
+	 * contains the number of new symbols generated to linearize products
+	 */
+	private int nbNewSymbols;
 
-    @Override
-    protected void checkId(StringBuffer s) throws ParseFormatException {
-        // Small check on the coefficient ID to make sure everything is ok
-        int cpt = 1;
-        if (s.charAt(0) == '~')
-            cpt = 2;
-        int varID = Integer.parseInt(s.substring(cpt));
-        if (varID > nbVars) {
-            throw new ParseFormatException(
-                    "Variable identifier larger than #variables in metadata.");
-        }
-    }
+	@Override
+	protected void readTerm(StringBuffer coeff, StringBuffer var)
+			throws IOException, ParseFormatException {
+		readInteger(coeff);
 
-    /**
-     * contains the number of new symbols generated to linearize products
-     */
-    private int nbNewSymbols;
+		skipSpaces();
 
-    @Override
-    protected void readTerm(StringBuffer coeff, StringBuffer var)
-            throws IOException, ParseFormatException {
-        readInteger(coeff);
-
-        skipSpaces();
-
-        var.setLength(0);
-        IVec<String> tmpLit = new Vec<String>();
-        StringBuffer tmpVar = new StringBuffer();
-        while (readIdentifier(tmpVar)) {
-            tmpLit = tmpLit.push(tmpVar.toString());
-            skipSpaces();
-        }
-        if (tmpLit.size() == 0)
-            throw new ParseFormatException("identifier expected");
-        if (tmpLit.size() == 1) {
-            // it is a "normal" term
-            var.append(tmpLit.last());
-            tmpLit.pop();
-        } else {
-            // it is a product term
-            try {
+		var.setLength(0);
+		IVec<String> tmpLit = new Vec<String>();
+		StringBuffer tmpVar = new StringBuffer();
+		while (readIdentifier(tmpVar)) {
+			tmpLit = tmpLit.push(tmpVar.toString());
+			skipSpaces();
+		}
+		if (tmpLit.size() == 0)
+			throw new ParseFormatException("identifier expected");
+		if (tmpLit.size() == 1) {
+			// it is a "normal" term
+			var.append(tmpLit.last());
+			tmpLit.pop();
+		} else {
+			// it is a product term
+			try {
 				var.append(linearizeProduct(tmpLit));
 			} catch (ContradictionException e) {
 				throw new ParseFormatException(e);
 			}
-        }
-    }
+		}
+	}
 
-    /**
-     * callback called when we read a term of a constraint
-     * 
-     * @param var
-     *                the identifier of the variable
-     * @param lits
-     *                a set of literals in DIMACS format in which var once
-     *                translated will be added.
-     */
-    protected void literalInAProduct(String var, IVecInt lits) {
-        int beginning = ((var.charAt(0) == '~') ? 2 : 1);
-        int id = Integer.parseInt(var.substring(beginning));
-        int lid = ((var.charAt(0) == '~') ? -1 : 1) * id;
-        lits.push(lid);
-    }
+	/**
+	 * callback called when we read a term of a constraint
+	 * 
+	 * @param var
+	 *            the identifier of the variable
+	 * @param lits
+	 *            a set of literals in DIMACS format in which var once
+	 *            translated will be added.
+	 */
+	protected void literalInAProduct(String var, IVecInt lits) {
+		int beginning = ((var.charAt(0) == '~') ? 2 : 1);
+		int id = Integer.parseInt(var.substring(beginning));
+		int lid = ((var.charAt(0) == '~') ? -1 : 1) * id;
+		lits.push(lid);
+	}
 
-    /**
-     * callback called when we read a term of a constraint
-     * 
-     * @param var
-     *                the identifier of the variable
-     * @param lits
-     *                a set of literals in DIMACS format in which var once
-     *                translated will be added.
-     */
-    protected void negateLiteralInAProduct(String var, IVecInt lits) {
-        int beginning = ((var.charAt(0) == '~') ? 2 : 1);
-        int id = Integer.parseInt(var.substring(beginning));
-        int lid = ((var.charAt(0) == '~') ? 1 : -1) * id;
-        lits.push(lid);
-    }
+	/**
+	 * callback called when we read a term of a constraint
+	 * 
+	 * @param var
+	 *            the identifier of the variable
+	 * @param lits
+	 *            a set of literals in DIMACS format in which var once
+	 *            translated will be added.
+	 */
+	protected void negateLiteralInAProduct(String var, IVecInt lits) {
+		int beginning = ((var.charAt(0) == '~') ? 2 : 1);
+		int id = Integer.parseInt(var.substring(beginning));
+		int lid = ((var.charAt(0) == '~') ? 1 : -1) * id;
+		lits.push(lid);
+	}
 
-    /**
-     * read the first comment line to get the number of variables and the number
-     * of constraints in the file calls metaData with the data that was read
-     * 
-     * @throws IOException
-     * @throws ParseException
-     */
-    @Override
-    protected void readMetaData() throws IOException, ParseFormatException {
-        char c;
-        String s;
+	/**
+	 * read the first comment line to get the number of variables and the number
+	 * of constraints in the file calls metaData with the data that was read
+	 * 
+	 * @throws IOException
+	 * @throws ParseException
+	 */
+	@Override
+	protected void readMetaData() throws IOException, ParseFormatException {
+		char c;
+		String s;
 
-        // get the number of variables and constraints
-        c = get();
-        if (c != '*')
-            throw new ParseFormatException(
-                    "First line of input file should be a comment");
-        s = readWord();
-        if (eof() || !"#variable=".equals(s))
-            throw new ParseFormatException(
-                    "First line should contain #variable= as first keyword");
+		// get the number of variables and constraints
+		c = get();
+		if (c != '*')
+			throw new ParseFormatException(
+					"First line of input file should be a comment");
+		s = readWord();
+		if (eof() || !"#variable=".equals(s))
+			throw new ParseFormatException(
+					"First line should contain #variable= as first keyword");
 
-        nbVars = Integer.parseInt(readWord());
-        nbNewSymbols = nbVars + 1;
+		nbVars = Integer.parseInt(readWord());
+		nbNewSymbols = nbVars + 1;
 
-        s = readWord();
-        if (eof() || !"#constraint=".equals(s))
-            throw new ParseFormatException(
-                    "First line should contain #constraint= as second keyword");
+		s = readWord();
+		if (eof() || !"#constraint=".equals(s))
+			throw new ParseFormatException(
+					"First line should contain #constraint= as second keyword");
 
-        nbConstr = Integer.parseInt(readWord());
-        charAvailable = false;
-        if (!eol()) {
-            String rest = in.readLine();
-            
-            if (rest!=null&&rest.indexOf("#product=") != -1) {
-                String[] splitted = rest.trim().split(" ");
-                if (splitted[0].equals("#product=")) {
-                    nbProducts = Integer.parseInt(splitted[1]);
-                    // nbVars += nbProducts;
-                    // nbConstr += 2 * nbProducts;
-                }
+		nbConstr = Integer.parseInt(readWord());
+		charAvailable = false;
+		if (!eol()) {
+			String rest = in.readLine();
 
-                // if (splitted[2].equals("sizeproduct="))
-                // readWord();
+			if (rest != null && rest.indexOf("#product=") != -1) {
+				String[] splitted = rest.trim().split(" ");
+				if (splitted[0].equals("#product=")) {
+					Integer.parseInt(splitted[1]);
+				}
 
-            }
-        }
-        // callback to transmit the data
-        metaData(nbVars, nbConstr);
-    }
+				// if (splitted[2].equals("sizeproduct="))
+				// readWord();
 
-    @Override
-    protected int translateVarToId(String var) {
-        int beginning = ((var.charAt(0) == '~') ? 2 : 1);
-        int id = Integer.parseInt(var.substring(beginning));
-        return ((var.charAt(0) == '~') ? -1 : 1) * id;
-    }
+			}
+		}
+		// callback to transmit the data
+		metaData(nbVars, nbConstr);
+	}
 
-    private String linearizeProduct(IVec<String> tmpLit)
-            throws ContradictionException {
-        tmpLit.sort(String.CASE_INSENSITIVE_ORDER);
-        String newVar = getProductVariable(tmpLit);
-        if (newVar == null) {
-            // generate a new symbol
-            newVar = "X" + String.valueOf(nbNewSymbols++);
-            // linearization proposed by O. Roussel (PB07)
-            // generate the clause
-            // product => newSymbol (this is a clause)
-            // not x1 or not x2 ... or not xn or newSymbol
-            productStore.put(newVar, tmpLit);
-            IVecInt newLits = new VecInt();
-            for (Iterator<String> iterator = tmpLit.iterator();iterator.hasNext();)
-                negateLiteralInAProduct(iterator.next(), newLits);
-            literalInAProduct(newVar, newLits);
-            solver.addClause(newLits);
-            // generate the PB-constraint
-            // newSymbol => product translated as
-            // x1+x2+x3...+xn-n*newSymbol>=0
-            newLits.clear();
-            IVec<BigInteger> newCoefs = new Vec<BigInteger>();
-            for (Iterator<String> iterator = tmpLit.iterator();iterator.hasNext();) {
-                literalInAProduct(iterator.next(), newLits);
-                newCoefs.push(BigInteger.ONE);
-            }
-            literalInAProduct(newVar, newLits);
-            newCoefs.push(new BigInteger(String.valueOf(-tmpLit.size())));
-            solver.addPseudoBoolean(newLits, newCoefs, true, BigInteger.ZERO);
-            // nbConstraintsRead += 2;
-        }
-        return newVar;
-    }
+	@Override
+	protected int translateVarToId(String var) {
+		int beginning = ((var.charAt(0) == '~') ? 2 : 1);
+		int id = Integer.parseInt(var.substring(beginning));
+		return ((var.charAt(0) == '~') ? -1 : 1) * id;
+	}
 
-    private Map<String, IVec<String>> productStore = new HashMap<String, IVec<String>>();
+	private String linearizeProduct(IVec<String> tmpLit)
+			throws ContradictionException {
+		tmpLit.sort(String.CASE_INSENSITIVE_ORDER);
+		String newVar = getProductVariable(tmpLit);
+		if (newVar == null) {
+			// generate a new symbol
+			newVar = "X" + String.valueOf(nbNewSymbols++);
+			// linearization proposed by O. Roussel (PB07)
+			// generate the clause
+			// product => newSymbol (this is a clause)
+			// not x1 or not x2 ... or not xn or newSymbol
+			productStore.put(newVar, tmpLit);
+			IVecInt newLits = new VecInt();
+			for (Iterator<String> iterator = tmpLit.iterator(); iterator
+					.hasNext();)
+				negateLiteralInAProduct(iterator.next(), newLits);
+			literalInAProduct(newVar, newLits);
+			solver.addClause(newLits);
+			// generate the PB-constraint
+			// newSymbol => product translated as
+			// x1+x2+x3...+xn-n*newSymbol>=0
+			newLits.clear();
+			IVec<BigInteger> newCoefs = new Vec<BigInteger>();
+			for (Iterator<String> iterator = tmpLit.iterator(); iterator
+					.hasNext();) {
+				literalInAProduct(iterator.next(), newLits);
+				newCoefs.push(BigInteger.ONE);
+			}
+			literalInAProduct(newVar, newLits);
+			newCoefs.push(new BigInteger(String.valueOf(-tmpLit.size())));
+			solver.addPseudoBoolean(newLits, newCoefs, true, BigInteger.ZERO);
+			// nbConstraintsRead += 2;
+		}
+		return newVar;
+	}
 
-    private String getProductVariable(IVec<String> lits) {
-        for (Map.Entry<String, IVec<String>> c : productStore.entrySet())
-            if (c.getValue().equals(lits))
-                return c.getKey();
-        return null;
-    }
+	private final Map<String, IVec<String>> productStore = new HashMap<String, IVec<String>>();
+
+	private String getProductVariable(IVec<String> lits) {
+		for (Map.Entry<String, IVec<String>> c : productStore.entrySet())
+			if (c.getValue().equals(lits))
+				return c.getKey();
+		return null;
+	}
 
 }
