946c4da2ac3c3f18a634c98aeba1250afba08f60
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2009-05-15 18:17:52 +0000

    Fixed ECJ warning.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@444 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

--- org.sat4j.core/src/main/java/org/sat4j/tools/Minimal4InclusionModel.java
+++ org.sat4j.core/src/main/java/org/sat4j/tools/Minimal4InclusionModel.java
@@ -1,30 +1,30 @@
 /*******************************************************************************
-* SAT4J: a SATisfiability library for Java Copyright (C) 2004-2008 Daniel Le Berre
-*
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Alternatively, the contents of this file may be used under the terms of
-* either the GNU Lesser General Public License Version 2.1 or later (the
-* "LGPL"), in which case the provisions of the LGPL are applicable instead
-* of those above. If you wish to allow use of your version of this file only
-* under the terms of the LGPL, and not to allow others to use your version of
-* this file under the terms of the EPL, indicate your decision by deleting
-* the provisions above and replace them with the notice and other provisions
-* required by the LGPL. If you do not delete the provisions above, a recipient
-* may use your version of this file under the terms of the EPL or the LGPL.
-* 
-* Based on the original MiniSat specification from:
-* 
-* An extensible SAT solver. Niklas Een and Niklas Sorensson. Proceedings of the
-* Sixth International Conference on Theory and Applications of Satisfiability
-* Testing, LNCS 2919, pp 502-518, 2003.
-*
-* See www.minisat.se for the original solver in C++.
-* 
-*******************************************************************************/
+ * SAT4J: a SATisfiability library for Java Copyright (C) 2004-2008 Daniel Le Berre
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU Lesser General Public License Version 2.1 or later (the
+ * "LGPL"), in which case the provisions of the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of the LGPL, and not to allow others to use your version of
+ * this file under the terms of the EPL, indicate your decision by deleting
+ * the provisions above and replace them with the notice and other provisions
+ * required by the LGPL. If you do not delete the provisions above, a recipient
+ * may use your version of this file under the terms of the EPL or the LGPL.
+ * 
+ * Based on the original MiniSat specification from:
+ * 
+ * An extensible SAT solver. Niklas Een and Niklas Sorensson. Proceedings of the
+ * Sixth International Conference on Theory and Applications of Satisfiability
+ * Testing, LNCS 2919, pp 502-518, 2003.
+ *
+ * See www.minisat.se for the original solver in C++.
+ * 
+ *******************************************************************************/
 package org.sat4j.tools;
 
 import org.sat4j.core.VecInt;
@@ -49,54 +49,53 @@ import org.sat4j.specs.TimeoutException;
  */
 public class Minimal4InclusionModel extends SolverDecorator<ISolver> {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    /**
-     * @param solver
-     */
-    public Minimal4InclusionModel(ISolver solver) {
-        super(solver);
-    }
+	/**
+	 * @param solver
+	 */
+	public Minimal4InclusionModel(ISolver solver) {
+		super(solver);
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.sat4j.ISolver#model()
-     */
-    @SuppressWarnings("null")
-    @Override
-    public int[] model() {
-        int[] prevmodel = null;
-        IVecInt vec = new VecInt();
-        IVecInt cube = new VecInt();
-        // backUp();
-        try {
-            do {
-                prevmodel = super.model();
-                vec.clear();
-                cube.clear();
-                for (int q : prevmodel) {
-                    if (q < 0) {
-                        vec.push(-q);
-                    } else {
-                        cube.push(q);
-                    }
-                }
-                addClause(vec);
-            } while (isSatisfiable(cube));
-        } catch (TimeoutException e) {
-            throw new IllegalStateException("Solver timed out");
-        } catch (ContradictionException e) {
-            // added trivial unsat clauses
-        }
-        // restore();
-        int[] newmodel = new int[vec.size()];
-        for (int i = 0, j = 0; i < prevmodel.length; i++) {
-            if (prevmodel[i] < 0) {
-                newmodel[j++] = prevmodel[i];
-            }
-        }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.sat4j.ISolver#model()
+	 */
+	@Override
+	public int[] model() {
+		int[] prevmodel = null;
+		IVecInt vec = new VecInt();
+		IVecInt cube = new VecInt();
+		// backUp();
+		try {
+			do {
+				prevmodel = super.model();
+				vec.clear();
+				cube.clear();
+				for (int q : prevmodel) {
+					if (q < 0) {
+						vec.push(-q);
+					} else {
+						cube.push(q);
+					}
+				}
+				addClause(vec);
+			} while (isSatisfiable(cube));
+		} catch (TimeoutException e) {
+			throw new IllegalStateException("Solver timed out");
+		} catch (ContradictionException e) {
+			// added trivial unsat clauses
+		}
+		// restore();
+		int[] newmodel = new int[vec.size()];
+		for (int i = 0, j = 0; i < prevmodel.length; i++) {
+			if (prevmodel[i] < 0) {
+				newmodel[j++] = prevmodel[i];
+			}
+		}
 
-        return newmodel;
-    }
+		return newmodel;
+	}
 }
