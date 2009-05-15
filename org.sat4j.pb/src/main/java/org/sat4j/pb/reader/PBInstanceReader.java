48ee06363ee398337ddd9ad9a9c624f5468497e3
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2009-05-15 18:45:08 +0000

    Removed references to GooOPBReader.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@451 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

--- org.sat4j.pb/src/main/java/org/sat4j/pb/reader/PBInstanceReader.java
+++ org.sat4j.pb/src/main/java/org/sat4j/pb/reader/PBInstanceReader.java
@@ -1,30 +1,30 @@
 /*******************************************************************************
-* SAT4J: a SATisfiability library for Java Copyright (C) 2004-2008 Daniel Le Berre
-*
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Alternatively, the contents of this file may be used under the terms of
-* either the GNU Lesser General Public License Version 2.1 or later (the
-* "LGPL"), in which case the provisions of the LGPL are applicable instead
-* of those above. If you wish to allow use of your version of this file only
-* under the terms of the LGPL, and not to allow others to use your version of
-* this file under the terms of the EPL, indicate your decision by deleting
-* the provisions above and replace them with the notice and other provisions
-* required by the LGPL. If you do not delete the provisions above, a recipient
-* may use your version of this file under the terms of the EPL or the LGPL.
-* 
-* Based on the pseudo boolean algorithms described in:
-* A fast pseudo-Boolean constraint solver Chai, D.; Kuehlmann, A.
-* Computer-Aided Design of Integrated Circuits and Systems, IEEE Transactions on
-* Volume 24, Issue 3, March 2005 Page(s): 305 - 317
-* 
-* and 
-* Heidi E. Dixon, 2004. Automating Pseudo-Boolean Inference within a DPLL 
-* Framework. Ph.D. Dissertation, University of Oregon.
-*******************************************************************************/
+ * SAT4J: a SATisfiability library for Java Copyright (C) 2004-2008 Daniel Le Berre
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU Lesser General Public License Version 2.1 or later (the
+ * "LGPL"), in which case the provisions of the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of the LGPL, and not to allow others to use your version of
+ * this file under the terms of the EPL, indicate your decision by deleting
+ * the provisions above and replace them with the notice and other provisions
+ * required by the LGPL. If you do not delete the provisions above, a recipient
+ * may use your version of this file under the terms of the EPL or the LGPL.
+ * 
+ * Based on the pseudo boolean algorithms described in:
+ * A fast pseudo-Boolean constraint solver Chai, D.; Kuehlmann, A.
+ * Computer-Aided Design of Integrated Circuits and Systems, IEEE Transactions on
+ * Volume 24, Issue 3, March 2005 Page(s): 305 - 317
+ * 
+ * and 
+ * Heidi E. Dixon, 2004. Automating Pseudo-Boolean Inference within a DPLL 
+ * Framework. Ph.D. Dissertation, University of Oregon.
+ *******************************************************************************/
 package org.sat4j.pb.reader;
 
 import java.io.FileNotFoundException;
@@ -48,81 +48,79 @@ import org.sat4j.specs.IProblem;
  */
 public class PBInstanceReader extends InstanceReader {
 
-    private GoodOPBReader opb;
+	private OPBReader2007 opb;
 
-    private Reader reader = null;
+	private Reader reader = null;
 
-    private final IPBSolver solver;
+	private final IPBSolver solver;
 
-    public PBInstanceReader(IPBSolver solver) {
-    	super(solver);
-        this.solver = solver;
-    }
+	public PBInstanceReader(IPBSolver solver) {
+		super(solver);
+		this.solver = solver;
+	}
 
+	private Reader getDefaultOPBReader() {
+		if (opb == null) {
+			opb = new OPBReader2007(solver);
+		}
+		return opb;
+	}
 
-    private Reader getDefaultOPBReader() {
-        if (opb == null) {
-            opb = new GoodOPBReader(solver);
-        }
-        return opb;
-    }
+	@Override
+	public IProblem parseInstance(String filename)
+			throws FileNotFoundException, ParseFormatException, IOException,
+			ContradictionException {
+		String fname;
+		boolean isHttp = false;
+		String tempFileName = "";
+		String prefix = "";
 
+		if (filename.startsWith("http://")) {
+			isHttp = true;
+			tempFileName = filename;
+			filename = filename.substring(filename.lastIndexOf('/'), filename
+					.length() - 1);
+		}
 
-    @Override
-    public IProblem parseInstance(String filename)
-            throws FileNotFoundException, ParseFormatException, IOException,
-            ContradictionException {
-        String fname;
-        boolean isHttp = false;
-        String tempFileName = "";
-        String prefix = "";
+		if (filename.indexOf(':') != -1) {
 
-        if (filename.startsWith("http://")) {
-            isHttp = true;
-            tempFileName = filename;
-            filename = filename.substring(filename.lastIndexOf('/'), filename
-                    .length() - 1);
-        }
+			String[] parts = filename.split(":");
+			filename = parts[1];
+			prefix = parts[0].toUpperCase(Locale.getDefault());
 
-        if (filename.indexOf(':') != -1) {
+		}
 
-            String[] parts = filename.split(":");
-            filename = parts[1];
-            prefix = parts[0].toUpperCase(Locale.getDefault());
+		if (filename.endsWith(".gz")) {
+			fname = filename.substring(0, filename.lastIndexOf('.'));
+		} else {
+			fname = filename;
+		}
+		if (fname.endsWith(".opb") || "PB".equals(prefix)) {
+			reader = getDefaultOPBReader();
+		} else {
+			return super.parseInstance(filename);
+		}
 
-        }
+		if (isHttp) {
+			return reader.parseInstance((new URL(tempFileName)).openStream());
+		}
+		return reader.parseInstance(filename);
+	}
 
-        if (filename.endsWith(".gz")) {
-            fname = filename.substring(0, filename.lastIndexOf('.'));
-        } else {
-            fname = filename;
-        }
-        if (fname.endsWith(".opb") || "PB".equals(prefix)) {
-            reader = getDefaultOPBReader();
-        } else {
-            return super.parseInstance(filename);
-        }
+	@Override
+	@Deprecated
+	public String decode(int[] model) {
+		return reader.decode(model);
+	}
 
-        if (isHttp) {
-            return reader.parseInstance((new URL(tempFileName)).openStream());
-        }
-        return reader.parseInstance(filename);
-    }
+	@Override
+	public void decode(int[] model, PrintWriter out) {
+		reader.decode(model, out);
+	}
 
-    @Override
-    @Deprecated
-    public String decode(int[] model) {
-        return reader.decode(model);
-    }
-
-    @Override
-    public void decode(int[] model, PrintWriter out) {
-        reader.decode(model, out);
-    }
-
-    @Override
-    public IProblem parseInstance(java.io.Reader in)
-            throws ParseFormatException, ContradictionException, IOException {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public IProblem parseInstance(java.io.Reader in)
+			throws ParseFormatException, ContradictionException, IOException {
+		throw new UnsupportedOperationException();
+	}
 }
