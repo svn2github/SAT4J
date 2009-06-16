ff26125e2939b1f7637829bf19e8356c3396e16d
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2009-06-16 15:52:27 +0000

    Fixed Sonar warnings + added save actions on project.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@469 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

--- org.sat4j.multicore/src/main/java/org/sat4j/multicore/MultiCoreLauncher.java
+++ org.sat4j.multicore/src/main/java/org/sat4j/multicore/MultiCoreLauncher.java
@@ -1,18 +1,22 @@
 package org.sat4j.multicore;
 
 import org.sat4j.BasicLauncher;
-import org.sat4j.multicore.SolverFactory;
 import org.sat4j.specs.ISolver;
 
 public class MultiCoreLauncher {
+
+	private MultiCoreLauncher() {
+		// hidden from other objects.
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		BasicLauncher<ISolver> lanceur = new BasicLauncher<ISolver>(SolverFactory.instance());
-        lanceur.run(args);
-        System.exit(lanceur.getExitCode().value());
+		BasicLauncher<ISolver> lanceur = new BasicLauncher<ISolver>(
+				SolverFactory.instance());
+		lanceur.run(args);
+		System.exit(lanceur.getExitCode().value());
 	}
 
 }
