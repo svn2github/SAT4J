33b316e49746480f69312997b110f05a9d658673
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2009-07-22 22:01:08 +0000

    Updated test case for reset functionality.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@499 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

--- org.sat4j.core/src/test/java/org/sat4j/minisat/core/BugReset.java
+++ org.sat4j.core/src/test/java/org/sat4j/minisat/core/BugReset.java
@@ -1,5 +1,7 @@
 package org.sat4j.minisat.core;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -46,4 +48,102 @@ public class BugReset {
 		solver.addClause(new VecInt(clause));
 		assertTrue(solver.isSatisfiable()); // ArrayIndexOutOfBoundsException
 	}
+
+	@Test
+	public void testWithReset() throws TimeoutException, ContradictionException {
+		ISolver solver = SolverFactory.newDefault();
+		int[] clause;
+		boolean res;
+
+		// prepare the solver to accept MAXVAR variables. MANDATORY
+		solver.newVar(4);
+		// not mandatory for SAT solving. MANDATORY for MAXSAT solving
+		solver.setExpectedNumberOfClauses(6);
+		// Feed the solver using Dimacs format, using arrays of int
+		// (best option to avoid dependencies on SAT4J IVecInt)
+
+		// test empty
+		res = solver.isSatisfiable();
+		assertTrue(res);
+		solver.reset();
+
+		// test one clause
+		solver.newVar(1);
+		clause = new int[] { -4 };
+		solver.addClause(new VecInt(clause));
+		res = solver.isSatisfiable();
+		assertTrue(res);
+		solver.reset();
+
+		// test multiply statements
+		solver.newVar(4);
+		addData(solver);
+		assertTrue(solver.isSatisfiable());
+
+		clause = new int[] { -4 };
+		solver.addClause(new VecInt(clause));
+		try {
+			addData(solver);
+			res = solver.isSatisfiable();
+		} catch (ContradictionException e) {
+			res = false;
+		}
+		assertFalse(res);
+		solver.reset();
+	}
+
+	private void addData(ISolver solver) throws ContradictionException {
+		int[] clause = new int[] { -1, -2, -3, 4 };
+		solver.addClause(new VecInt(clause));
+
+		clause = new int[] { 1 };
+		solver.addClause(new VecInt(clause));
+
+		clause = new int[] { 2 };
+		solver.addClause(new VecInt(clause));
+
+		clause = new int[] { 3 };
+		solver.addClause(new VecInt(clause));
+
+	}
+
+	@Test
+	public void problemTest() throws TimeoutException, ContradictionException {
+		ISolver solver = SolverFactory.newDefault();
+		solver.setTimeout(3600);
+		solver.newVar(4);
+		solver.setExpectedNumberOfClauses(5);
+
+		for (int i = 0; i < 10; i++) {
+			solve(solver, new int[] {}, true);
+			solve(solver, new int[] { -4 }, false);
+		}
+	}
+
+	private void solve(ISolver solver, int[] clause, boolean value)
+			throws ContradictionException, TimeoutException {
+		boolean res = true;
+		try {
+			if (clause.length > 0)
+				solver.addClause(new VecInt(clause));
+			clause = new int[] { -1, 2, 4 };
+			solver.addClause(new VecInt(clause));
+			clause = new int[] { 1, -2 };
+			solver.addClause(new VecInt(clause));
+			clause = new int[] { 1, 2 };
+			solver.addClause(new VecInt(clause));
+			clause = new int[] { -1, -2 };
+			solver.addClause(new VecInt(clause));
+
+		} catch (ContradictionException e) {
+			res = false;
+		}
+		if (res) {
+			res = solver.isSatisfiable();
+		}
+		solver.reset();
+		solver.clearLearntClauses();
+		assertEquals(res, value);
+	}
+
 }
