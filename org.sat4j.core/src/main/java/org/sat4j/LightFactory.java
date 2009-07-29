a181d1eeb13b9b53e519e4220c70602ad5552465
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2009-07-29 20:59:00 +0000

    Reduced to one unique class per strategy.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@507 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

--- org.sat4j.core/src/main/java/org/sat4j/LightFactory.java
+++ org.sat4j.core/src/main/java/org/sat4j/LightFactory.java
@@ -30,14 +30,12 @@ package org.sat4j;
 import org.sat4j.core.ASolverFactory;
 import org.sat4j.minisat.constraints.MixedDataStructureDanielWL;
 import org.sat4j.minisat.core.DataStructureFactory;
-import org.sat4j.minisat.core.IOrder;
 import org.sat4j.minisat.core.SearchParams;
 import org.sat4j.minisat.core.Solver;
 import org.sat4j.minisat.learning.MiniSATLearning;
 import org.sat4j.minisat.orders.RSATPhaseSelectionStrategy;
 import org.sat4j.minisat.orders.VarOrderHeap;
 import org.sat4j.minisat.restarts.ArminRestarts;
-import org.sat4j.minisat.restarts.MiniSATRestarts;
 import org.sat4j.minisat.uip.FirstUIP;
 import org.sat4j.specs.ISolver;
 
@@ -65,51 +63,20 @@ public class LightFactory extends ASolverFactory<ISolver> {
 
 	@Override
 	public ISolver defaultSolver() {
-		return newMiniLearningHeapRsatExpSimpBiere();
-	}
-
-	@Override
-	public ISolver lightSolver() {
-		return newMiniLearningHeapRsatExpSimpBiere();
-	}
-
-	public static Solver<DataStructureFactory> newMiniLearningHeapRsatExpSimpBiere() {
-		Solver<DataStructureFactory> solver = newMiniLearningHeapRsatExpSimp();
-		solver.setRestartStrategy(new ArminRestarts());
+		MiniSATLearning<DataStructureFactory> learning = new MiniSATLearning<DataStructureFactory>();
+		Solver<DataStructureFactory> solver = new Solver<DataStructureFactory>(
+				new FirstUIP(), learning, new MixedDataStructureDanielWL(),
+				new VarOrderHeap(new RSATPhaseSelectionStrategy()),
+				new ArminRestarts());
+		learning.setSolver(solver);
+		solver.setSimplifier(solver.EXPENSIVE_SIMPLIFICATION);
 		solver.setSearchParams(new SearchParams(1.1, 100));
 		return solver;
 	}
 
-	public static Solver<DataStructureFactory> newMiniLearningHeapRsatExpSimp() {
-		Solver<DataStructureFactory> solver = newMiniLearningHeapExpSimp();
-		solver.setOrder(new VarOrderHeap(new RSATPhaseSelectionStrategy()));
-		return solver;
-	}
-
-	public static Solver<DataStructureFactory> newMiniLearningHeapExpSimp() {
-		Solver<DataStructureFactory> solver = newMiniLearningHeap();
-		solver.setSimplifier(solver.EXPENSIVE_SIMPLIFICATION);
-		return solver;
-	}
-
-	public static Solver<DataStructureFactory> newMiniLearningHeap() {
-		return newMiniLearningHeap(new MixedDataStructureDanielWL());
-	}
-
-	public static Solver<DataStructureFactory> newMiniLearningHeap(
-			DataStructureFactory dsf) {
-		return newMiniLearning(dsf, new VarOrderHeap());
-	}
-
-	public static Solver<DataStructureFactory> newMiniLearning(
-			DataStructureFactory dsf, IOrder order) {
-		// LimitedLearning<DataStructureFactory> learning = new
-		// PercentLengthLearning<DataStructureFactory>(10);
-		MiniSATLearning<DataStructureFactory> learning = new MiniSATLearning<DataStructureFactory>();
-		Solver<DataStructureFactory> solver = new Solver<DataStructureFactory>(
-				new FirstUIP(), learning, dsf, order, new MiniSATRestarts());
-		learning.setSolver(solver);
-		return solver;
+	@Override
+	public ISolver lightSolver() {
+		return defaultSolver();
 	}
 
 	public static void main(final String[] args) {
