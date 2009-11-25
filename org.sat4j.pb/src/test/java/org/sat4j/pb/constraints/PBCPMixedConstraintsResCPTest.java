e0588be636be92eb2e4417e440ee305513c00a24
parrain <parrain@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2009-11-25 14:10:13 +0000

    new Solver switching from resolution to cutting planes added
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@567 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

---
+++ org.sat4j.pb/src/test/java/org/sat4j/pb/constraints/PBCPMixedConstraintsResCPTest.java
@@ -0,0 +1,19 @@
+package org.sat4j.pb.constraints;
+
+import org.sat4j.pb.IPBSolver;
+import org.sat4j.pb.SolverFactory;
+
+public class PBCPMixedConstraintsResCPTest extends
+		AbstractPseudoBooleanAndPigeonHoleTest {
+
+	public PBCPMixedConstraintsResCPTest(String arg) {
+		super(arg);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected IPBSolver createSolver() {
+		return SolverFactory.newPBCPMixedConstraintsResCP();
+	}
+
+}
