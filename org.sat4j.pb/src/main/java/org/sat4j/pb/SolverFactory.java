8c2280070c507a92a56f41fefa69822360e5a9a6
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2010-07-17 16:47:43 +0000

    Added a resolution based solver with simple simplification.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@730 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

--- org.sat4j.pb/src/main/java/org/sat4j/pb/SolverFactory.java
+++ org.sat4j.pb/src/main/java/org/sat4j/pb/SolverFactory.java
@@ -557,6 +557,20 @@ public class SolverFactory extends ASolverFactory<IPBSolver> {
 	/**
 	 * Resolution based solver (i.e. classic SAT solver able to handle generic
 	 * constraints. No specific inference mechanism). Uses glucose based memory
+	 * management.
+	 * 
+	 * @return the best available resolution based solver of the library.
+	 */
+	public static IPBSolver newSimpleSimplification() {
+		PBSolverResolution solver = newCompetPBResWLMixedConstraintsObjectiveExpSimp();
+		solver.setLearnedConstraintsDeletionStrategy(solver.glucose);
+		solver.setSimplifier(solver.SIMPLE_SIMPLIFICATION);
+		return solver;
+	}
+
+	/**
+	 * Resolution based solver (i.e. classic SAT solver able to handle generic
+	 * constraints. No specific inference mechanism). Uses glucose based memory
 	 * management. Uses a simple restart strategy (original Minisat's one).
 	 * 
 	 * @return the best available resolution based solver of the library.
