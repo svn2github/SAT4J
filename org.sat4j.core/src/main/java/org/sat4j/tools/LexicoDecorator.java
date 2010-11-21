51175a1704cf6c54f99af0279b90e5978f8b741a
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2010-11-21 17:41:12 +0000

    New solver decorator allowing to perform lexicographic optimization.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@817 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

---
+++ org.sat4j.core/src/main/java/org/sat4j/tools/LexicoDecorator.java
@@ -0,0 +1,142 @@
+package org.sat4j.tools;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.sat4j.core.VecInt;
+import org.sat4j.specs.ContradictionException;
+import org.sat4j.specs.IConstr;
+import org.sat4j.specs.IOptimizationProblem;
+import org.sat4j.specs.ISolver;
+import org.sat4j.specs.IVecInt;
+import org.sat4j.specs.IteratorInt;
+import org.sat4j.specs.TimeoutException;
+
+public class LexicoDecorator<T extends ISolver> extends SolverDecorator<T>
+		implements IOptimizationProblem {
+
+	protected final List<IVecInt> criteria = new ArrayList<IVecInt>();
+
+	protected int currentCriterion = 0;
+
+	private IConstr prevConstr;
+
+	private int currentValue = -1;
+
+	protected int[] prevfullmodel;
+	protected boolean[] prevboolmodel;
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public LexicoDecorator(T solver) {
+		super(solver);
+	}
+
+	public void addCriterion(IVecInt literals) {
+		IVecInt copy = new VecInt(literals.size());
+		literals.copyTo(copy);
+		criteria.add(copy);
+	}
+
+	public boolean admitABetterSolution() throws TimeoutException {
+		return admitABetterSolution(VecInt.EMPTY);
+	}
+
+	public boolean admitABetterSolution(IVecInt assumps)
+			throws TimeoutException {
+		if (decorated().isSatisfiable(assumps)) {
+			prevboolmodel = new boolean[nVars()];
+			for (int i = 0; i < nVars(); i++) {
+				prevboolmodel[i] = decorated().model(i + 1);
+			}
+			calculateObjective();
+			prevfullmodel = decorated().model();
+			return true;
+		}
+		if (currentCriterion < criteria.size() - 1) {
+			if (prevConstr != null) {
+				super.removeConstr(prevConstr);
+				prevConstr = null;
+			}
+			try {
+				super.addAtMost(criteria.get(currentCriterion), currentValue);
+			} catch (ContradictionException e) {
+				throw new IllegalStateException(e);
+			}
+			if (isVerbose()) {
+				System.out.println(getLogPrefix()
+						+ "Found optimal criterion number "
+						+ (currentCriterion + 1));
+			}
+			currentCriterion++;
+			calculateObjective();
+			return true;
+		}
+		if (prevConstr != null) {
+			super.removeConstr(prevConstr);
+			prevConstr = null;
+		}
+		return false;
+	}
+
+	@Override
+	public int[] model() {
+		return prevfullmodel;
+	}
+
+	@Override
+	public boolean model(int var) {
+		return prevboolmodel[var - 1];
+	}
+
+	public boolean hasNoObjectiveFunction() {
+		return false;
+	}
+
+	public boolean nonOptimalMeansSatisfiable() {
+		return true;
+	}
+
+	public Number calculateObjective() {
+		currentValue = evaluate();
+		return currentValue;
+	}
+
+	public Number getObjectiveValue() {
+		return currentValue;
+	}
+
+	public void forceObjectiveValueTo(Number forcedValue)
+			throws ContradictionException {
+		throw new UnsupportedOperationException();
+	}
+
+	public void discard() throws ContradictionException {
+		discardCurrentSolution();
+
+	}
+
+	public void discardCurrentSolution() throws ContradictionException {
+		if (prevConstr != null) {
+			super.removeSubsumedConstr(prevConstr);
+		}
+		prevConstr = super.addAtMost(criteria.get(currentCriterion),
+				currentValue - 1);
+
+	}
+
+	private int evaluate() {
+		int value = 0;
+		for (IteratorInt it = criteria.get(currentCriterion).iterator(); it
+				.hasNext();) {
+			if (prevboolmodel[it.next() - 1]) {
+				value++;
+			}
+		}
+		return value;
+	}
+
+}
