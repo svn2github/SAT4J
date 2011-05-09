a0f37d489195fd5120f6c99fbeec38a37a0d2c40
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2011-05-09 19:35:03 +0000

    Test case showing the problem in bug SAT-22.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@945 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

---
+++ org.sat4j.pb/src/test/java/org/sat4j/pb/BugSAT22.java
@@ -0,0 +1,92 @@
+package org.sat4j.pb;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Test;
+import org.sat4j.pb.tools.DependencyHelper;
+import org.sat4j.pb.tools.WeightedObject;
+import org.sat4j.specs.ContradictionException;
+import org.sat4j.specs.IVec;
+import org.sat4j.specs.TimeoutException;
+
+public class BugSAT22 {
+	@Test
+	public void testSimpleResolverUnitFirst() throws ContradictionException,
+			TimeoutException {
+		IPBSolver solver = SolverFactory.newEclipseP2();
+		DependencyHelper<Named, String> helper = new DependencyHelper<Named, String>(
+				solver, false);
+		Set<Named> slice = new HashSet<Named>();
+		Named A1 = new Named("A1");
+		slice.add(A1);
+		Named A2 = new Named("A2");
+		slice.add(A2);
+		Named B = new Named("B");
+		slice.add(B);
+		Named X = new Named("X");
+		// base
+		helper.setTrue(X, "Build");
+		// weighted
+		List<WeightedObject<Named>> wos = new ArrayList<WeightedObject<Named>>();
+		wos.add(WeightedObject.newWO(A2, 1));
+		wos.add(WeightedObject.newWO(A1, 2));
+		helper.setObjectiveFunction(wos.toArray(new WeightedObject[0]));
+		// depends
+		helper.or("a", X, new Named[] { A1, A2 });
+		helper.or("b", X, new Named[] { B });
+		// solve
+		assertTrue(helper.hasASolution());
+		IVec<Named> solution = helper.getSolution();
+		assertEquals(3, solution.size());
+		assertTrue(solution.contains(B));
+		assertTrue(solution.contains(X));
+		assertTrue(solution.contains(A2));
+	}
+
+	@Test
+	public void testSimpleResolverUnitLast() throws ContradictionException,
+			TimeoutException {
+		IPBSolver solver = SolverFactory.newEclipseP2();
+		DependencyHelper<Named, String> helper = new DependencyHelper<Named, String>(
+				solver, false);
+		Set<Named> slice = new HashSet<Named>();
+		Named A1 = new Named("A1");
+		slice.add(A1);
+		Named A2 = new Named("A2");
+		slice.add(A2);
+		Named B = new Named("B");
+		slice.add(B);
+		Named X = new Named("X");
+		// weighted
+		List<WeightedObject<Named>> wos = new ArrayList<WeightedObject<Named>>();
+		wos.add(WeightedObject.newWO(A2, 1));
+		wos.add(WeightedObject.newWO(A1, 2));
+		helper.setObjectiveFunction(wos.toArray(new WeightedObject[0]));
+		// depends
+		helper.or("a", X, new Named[] { A1, A2 });
+		helper.or("b", X, new Named[] { B });
+		// base
+		helper.setTrue(X, "Build");
+		// solve
+		assertTrue(helper.hasASolution());
+		IVec<Named> solution = helper.getSolution();
+		assertEquals(3, solution.size());
+		assertTrue(solution.contains(B));
+		assertTrue(solution.contains(X));
+		assertTrue(solution.contains(A2));
+	}
+
+	public class Named {
+		public String name;
+
+		public Named(String name) {
+			this.name = name;
+		}
+	}
+}
