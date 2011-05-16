74f1c600543e504d3fad71a3d2a099ec28b34a9c
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2011-05-16 21:35:36 +0000

    Fix to support #parseInstance(InputStream)
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@958 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

--- org.sat4j.pb/src/test/java/org/sat4j/pb/GoodOPBReader.java
+++ org.sat4j.pb/src/test/java/org/sat4j/pb/GoodOPBReader.java
@@ -29,6 +29,7 @@ package org.sat4j.pb;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.LineNumberReader;
 import java.io.PrintWriter;
 import java.io.Serializable;
@@ -221,8 +222,8 @@ public class GoodOPBReader extends Reader implements Serializable {
 	}
 
 	@Override
-	public IProblem parseInstance(InputStream in) throws ParseFormatException,
-			ContradictionException, IOException {
-		throw new UnsupportedOperationException();
+	public IProblem parseInstance(final InputStream in)
+			throws ParseFormatException, ContradictionException, IOException {
+		return parseInstance(new InputStreamReader(in));
 	}
 }
