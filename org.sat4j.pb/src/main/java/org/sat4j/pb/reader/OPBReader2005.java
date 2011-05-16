95592fcf7c3b2f02da4ce446013ba0767cda2b2b
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2011-05-16 21:43:44 +0000

    Fix to support #parseInstance(InputStream) until we reimplement completely the reader.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@959 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

--- org.sat4j.pb/src/main/java/org/sat4j/pb/reader/OPBReader2005.java
+++ org.sat4j.pb/src/main/java/org/sat4j/pb/reader/OPBReader2005.java
@@ -54,6 +54,7 @@ package org.sat4j.pb.reader;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.LineNumberReader;
 import java.io.PrintWriter;
 import java.io.Serializable;
@@ -745,10 +746,8 @@ public class OPBReader2005 extends Reader implements Serializable {
 	}
 
 	@Override
-	public IProblem parseInstance(InputStream in) throws ParseFormatException,
-			ContradictionException, IOException {
-		throw new UnsupportedOperationException(
-				"OPB files are read from a Reader, not an InputStream (need to be fixed)");
+	public IProblem parseInstance(final InputStream in)
+			throws ParseFormatException, ContradictionException, IOException {
+		return parseInstance(new InputStreamReader(in));
 	}
-
 }
