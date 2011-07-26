d9c19a9e422c2838cd5fc2eccee1fce3a59ff58a
parrain <parrain@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2011-07-26 17:54:35 +0000

    Test case for weighted maxsat solvers dealing with only hard clauses. Thanks to Fatih Turkmen.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@965 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

---
+++ org.sat4j.maxsat/src/test/java/org/sat4j/maxsat/BugFatih2.java
@@ -0,0 +1,55 @@
+package org.sat4j.maxsat;
+
+import org.junit.Test;
+import org.sat4j.core.VecInt;
+import org.sat4j.specs.ContradictionException;
+import org.sat4j.specs.TimeoutException;
+import org.sat4j.tools.ModelIterator;
+import org.sat4j.tools.OptToSatAdapter;
+
+public class BugFatih2 {
+
+	@Test
+	public void testBugReport() throws ContradictionException, TimeoutException {
+		// ModelIterator solver = new ModelIterator(new OptToSatAdapter(
+		// new MaxSatDecorator(SolverFactory.newDefault())));
+		WeightedMaxSatDecorator maxSatSolver = new WeightedMaxSatDecorator(
+				org.sat4j.maxsat.SolverFactory.newDefault());
+		ModelIterator solver = new ModelIterator(new OptToSatAdapter(maxSatSolver));
+		System.out.println("Taille de voc : " + solver.nVars());
+		solver.newVar(13);
+		solver.setExpectedNumberOfClauses(24);
+		maxSatSolver.addHardClause(new VecInt(new int[] { -1 }));
+		maxSatSolver.addHardClause(new VecInt(new int[] { -2 }));
+		maxSatSolver.addHardClause(new VecInt(new int[] { -3, 4 }));
+		maxSatSolver.addHardClause(new VecInt(new int[] { -3, 5 }));
+		maxSatSolver.addHardClause(new VecInt(new int[] { -3, 6 }));
+		maxSatSolver.addHardClause(new VecInt(new int[] { -1, 7 }));
+		maxSatSolver.addHardClause(new VecInt(new int[] { -2, 6 }));
+		maxSatSolver.addHardClause(new VecInt(new int[] { -4, 3 }));
+		maxSatSolver.addHardClause(new VecInt(new int[] { -5, 3 }));
+		maxSatSolver.addHardClause(new VecInt(new int[] { -6, 3, 2 }));
+		maxSatSolver.addHardClause(new VecInt(new int[] { -7, 1 }));
+		maxSatSolver.addHardClause(new VecInt(new int[] { 3, -1, 8 }));
+		maxSatSolver.addHardClause(new VecInt(new int[] { -3, 1, 8 }));
+		maxSatSolver.addHardClause(new VecInt(new int[] { -3, -1, 9 }));
+		maxSatSolver.addHardClause(new VecInt(new int[] { -9 }));
+		maxSatSolver.addHardClause(new VecInt(new int[] { 1, -2, 10 }));
+		maxSatSolver.addHardClause(new VecInt(new int[] { -1, 2, 10 }));
+		maxSatSolver.addHardClause(new VecInt(new int[] { -1, -2, 11 }));
+		maxSatSolver.addHardClause(new VecInt(new int[] { -10 }));
+		maxSatSolver.addHardClause(new VecInt(new int[] { -11 }));
+		maxSatSolver.addHardClause(new VecInt(new int[] { 3, -1, 12 }));
+		maxSatSolver.addHardClause(new VecInt(new int[] { -3, 1, 12 }));
+		maxSatSolver.addHardClause(new VecInt(new int[] { -3, -1, 13 }));
+		maxSatSolver.addHardClause(new VecInt(new int[] { -13 }));
+		System.out.println("Taille de voc : " + solver.nVars());
+		while (solver.isSatisfiable()) {
+			System.out.println("Taille du modèle : " + solver.model().length);
+			for (int i = 1; i <= solver.model().length; i++) {
+				System.out.print(solver.model(i) + " ");
+			}
+			System.out.println();
+		}
+	}
+}
