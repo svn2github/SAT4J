0fc9c0b217a25c12bb8712f7cf8d90ee38a14a6e
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2011-09-20 12:57:26 +0000

    should have fixed incorrectness bug in the solver.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@1008 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

--- org.sat4j.core/src/main/java/org/sat4j/minisat/core/Solver.java
+++ org.sat4j.core/src/main/java/org/sat4j/minisat/core/Solver.java
@@ -531,9 +531,8 @@ public class Solver<D extends DataStructureFactory> implements ISolver,
 						}
 					}
 				}
-				prevConfl = confl;
 			}
-
+			prevConfl = confl;
 			// select next reason to look at
 			do {
 				p = trail.last();
@@ -831,24 +830,28 @@ public class Solver<D extends DataStructureFactory> implements ISolver,
 			assert lvoc.getReason(q) != null;
 			Constr c = lvoc.getReason(q);
 			lanalyzestack.pop();
-			if (!c.canBePropagatedMultipleTimes()) {
-				for (int i = 0; i < c.size(); i++) {
-					int l = c.get(i);
-					if (!seen[var(l)] && lvoc.isFalsified(l)
-							&& lvoc.getLevel(l) != 0) {
-						if (lvoc.getReason(l) == null) {
-							for (int j = top; j < lanalyzetoclear.size(); j++)
-								seen[lanalyzetoclear.get(j) >> 1] = false;
-							lanalyzetoclear
-									.shrink(lanalyzetoclear.size() - top);
-							return false;
-						}
-						seen[l >> 1] = true;
-						lanalyzestack.push(l);
-						lanalyzetoclear.push(l);
+			if (c.canBePropagatedMultipleTimes()) {
+				for (int j = top; j < lanalyzetoclear.size(); j++)
+					seen[lanalyzetoclear.get(j) >> 1] = false;
+				lanalyzetoclear.shrink(lanalyzetoclear.size() - top);
+				return false;
+			}
+			for (int i = 0; i < c.size(); i++) {
+				int l = c.get(i);
+				if (!seen[var(l)] && lvoc.isFalsified(l)
+						&& lvoc.getLevel(l) != 0) {
+					if (lvoc.getReason(l) == null) {
+						for (int j = top; j < lanalyzetoclear.size(); j++)
+							seen[lanalyzetoclear.get(j) >> 1] = false;
+						lanalyzetoclear.shrink(lanalyzetoclear.size() - top);
+						return false;
 					}
+					seen[l >> 1] = true;
+					lanalyzestack.push(l);
+					lanalyzetoclear.push(l);
 				}
 			}
+
 		}
 
 		return true;
