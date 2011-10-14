d4321c4354261245b3d15c7d8231dc4e14a69b3b
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2011-10-14 09:01:48 +0000

    Moved ConstrGroup to org.sat4j.core to avoid cycles (thanks sonar).
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@1051 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

--- org.sat4j.core/src/test/java/org/sat4j/TestConstrGroup.java
+++ org.sat4j.core/src/test/java/org/sat4j/TestConstrGroup.java
@@ -32,9 +32,9 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.sat4j.core.ConstrGroup;
 import org.sat4j.core.VecInt;
 import org.sat4j.minisat.SolverFactory;
-import org.sat4j.specs.ConstrGroup;
 import org.sat4j.specs.ContradictionException;
 import org.sat4j.specs.ISolver;
 import org.sat4j.specs.IVecInt;
