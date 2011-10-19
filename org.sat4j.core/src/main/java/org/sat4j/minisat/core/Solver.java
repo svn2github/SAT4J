18646faa5b2b664d2ca45e3c8a22c746f993e8f9
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2011-10-19 07:43:03 +0000

    Remove notion of wall in glucose.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@1063 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

--- org.sat4j.core/src/main/java/org/sat4j/minisat/core/Solver.java
+++ org.sat4j.core/src/main/java/org/sat4j/minisat/core/Solver.java
@@ -1468,7 +1468,6 @@ public class Solver<D extends DataStructureFactory> implements ISolver,
 		private static final long serialVersionUID = 1L;
 		private int[] flags = new int[0];
 		private int flag = 0;
-		private int wall = 0;
 
 		private final ConflictTimer clauseManagement = new ConflictTimerAdapter(
 				1000) {
@@ -1483,9 +1482,6 @@ public class Solver<D extends DataStructureFactory> implements ISolver,
 				nbconflict += bound();
 				if (nbconflict >= nextbound) {
 					nextbound += INC_CLAUSE;
-					if (nextbound > wall) {
-						nextbound = wall;
-					}
 					nbconflict = 0;
 					needToReduceDB = true;
 				}
@@ -1535,7 +1531,6 @@ public class Solver<D extends DataStructureFactory> implements ISolver,
 
 		public void init() {
 			final int howmany = voc.nVars();
-			wall = constrs.size() > 10000 ? constrs.size() : 10000;
 			if (flags.length <= howmany) {
 				flags = new int[howmany + 1];
 			}
