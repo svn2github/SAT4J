a5dafa5fdb481cee9ec1acd38f673a6110128a8f
sroussel <sroussel@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2011-10-20 08:20:43 +0000

    IN PROGRESS - issue SAT-38: Investigate usage of plain CNF rather than custom cardinality of PB constraints http://jira.ow2.org/browse/SAT-38 First implementation of SAT-38
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@1078 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

--- org.sat4j.core/src/test/java/org/sat4j/tools/TestClausalCardinalities.java
+++ org.sat4j.core/src/test/java/org/sat4j/tools/TestClausalCardinalities.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertNotNull;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.sat4j.core.ConstrGroup;
 import org.sat4j.core.VecInt;
 import org.sat4j.minisat.SolverFactory;
 import org.sat4j.specs.ContradictionException;
@@ -20,23 +21,36 @@ public class TestClausalCardinalities {
 
 	@Before
 	public void setUp() {
-		solver = new ClausalCardinalitiesDecorator<ISolver>(SolverFactory
-				.newDefault());
+		solver = new ClausalCardinalitiesDecorator<ISolver>(
+				SolverFactory.newDefault());
 	}
 
 	@Test
 	public void testSimpleCardCase() throws ContradictionException,
 			TimeoutException {
 		solver.newVar(5);
+
+		boolean debug = false;
 		IVecInt clause = new VecInt();
 		clause.push(1).push(2).push(3).push(4).push(5);
+
 		IConstr constr1 = solver.addClause(clause);
 		assertNotNull(constr1);
+
 		IConstr constr2 = solver.addAtMost(clause, 1);
 		assertNotNull(constr2);
+
+		if (debug) {
+			for (int i = 0; i < constr2.size(); i++) {
+				System.out.println(((ConstrGroup) constr2).getConstr(i));
+			}
+		}
+
 		ModelIterator iterator = new ModelIterator(solver);
 		int[] model = null;
 		int cpt = 0;
+
+		System.out.println("testSimpleCardCase models AMO + clause");
 		while (iterator.isSatisfiable()) {
 			model = iterator.model();
 			assertNotNull(model);
@@ -44,6 +58,144 @@ public class TestClausalCardinalities {
 			cpt++;
 		}
 		assertEquals(5, cpt);
+
+	}
+
+	@Test
+	public void testSimpleCardCase2Power() throws ContradictionException,
+			TimeoutException {
+		solver.newVar(4);
+
+		boolean debug = false;
+		IVecInt clause = new VecInt();
+		clause.push(1).push(2).push(3).push(4);
+
+		IConstr constr1 = solver.addClause(clause);
+		assertNotNull(constr1);
+
+		IConstr constr2 = solver.addAtMost(clause, 1);
+		assertNotNull(constr2);
+
+		if (debug) {
+			for (int i = 0; i < constr2.size(); i++) {
+				System.out.println(((ConstrGroup) constr2).getConstr(i));
+			}
+		}
+
+		ModelIterator iterator = new ModelIterator(solver);
+		int[] model = null;
+		int cpt = 0;
+
+		System.out.println("testSimpleCardCase2Power models AMO + clause");
+		while (iterator.isSatisfiable()) {
+			model = iterator.model();
+			assertNotNull(model);
+			System.out.println(new VecInt(model));
+			cpt++;
+		}
+		assertEquals(4, cpt);
+
+	}
+
+	@Test
+	public void testSimpleCardCaseAMO() throws ContradictionException,
+			TimeoutException {
+		solver.newVar(5);
+
+		boolean debug = false;
+		IVecInt clause = new VecInt();
+		clause.push(1).push(2).push(3).push(4).push(5);
+
+		IConstr constr2 = solver.addAtMost(clause, 1);
+		assertNotNull(constr2);
+
+		if (debug) {
+			System.out.println("Constraintes AMO");
+			for (int i = 0; i < constr2.size(); i++) {
+				System.out.println(((ConstrGroup) constr2).getConstr(i));
+			}
+		}
+
+		ModelIterator iterator = new ModelIterator(solver);
+		int[] model = null;
+		int cpt = 0;
+		System.out.println("testSimpleCardCase models AMO");
+		while (iterator.isSatisfiable()) {
+			model = iterator.model();
+			assertNotNull(model);
+			System.out.println(new VecInt(model));
+			cpt++;
+		}
+		assertEquals(6, cpt);
+
+	}
+
+	@Test
+	public void testSimpleCardCaseAMOWith8Variables()
+			throws ContradictionException, TimeoutException {
+		solver.newVar(8);
+
+		boolean debug = true;
+		IVecInt clause = new VecInt();
+		clause.push(1).push(2).push(3).push(4).push(5).push(6).push(7).push(8);
+
+		IConstr constr2 = solver.addAtMost(clause, 1);
+		assertNotNull(constr2);
+
+		if (debug) {
+			System.out.println("Constraintes AMO 8 variables");
+			for (int i = 0; i < constr2.size(); i++) {
+				System.out.println(((ConstrGroup) constr2).getConstr(i));
+			}
+		}
+
+		ModelIterator iterator = new ModelIterator(solver);
+		int[] model = null;
+		System.out.println("testSimpleCardCase models AMO 8 variables");
+		int cpt = 0;
+		while (iterator.isSatisfiable()) {
+			model = iterator.model();
+			assertNotNull(model);
+			System.out.println(new VecInt(model));
+			cpt++;
+		}
+		assertEquals(9, cpt);
+
+	}
+
+	@Test
+	public void testSimpleCardCaseEO() throws ContradictionException,
+			TimeoutException {
+		solver.newVar(5);
+
+		boolean debug = false;
+		IVecInt clause = new VecInt();
+		clause.push(1).push(2).push(3).push(4).push(5);
+
+		IConstr constr = solver.addExactly(clause, 1);
+		assertNotNull(constr);
+
+		assertEquals(16, constr.size());
+
+		if (debug) {
+			System.out.println("Constraintes EO");
+			for (int i = 0; i < constr.size(); i++) {
+				System.out.println(((ConstrGroup) constr).getConstr(i));
+			}
+		}
+
+		ModelIterator iterator = new ModelIterator(solver);
+		int[] model = null;
+		int cpt = 0;
+		System.out.println("testSimpleCardCase models EO");
+		while (iterator.isSatisfiable()) {
+			model = iterator.model();
+			assertNotNull(model);
+			System.out.println(new VecInt(model));
+			cpt++;
+		}
+		assertEquals(5, cpt);
+
 	}
 
 	@Test
@@ -59,12 +211,127 @@ public class TestClausalCardinalities {
 		ModelIterator iterator = new ModelIterator(solver);
 		int[] model = null;
 		int cpt = 0;
+		System.out
+				.println("testSimpleCardCaseFor2 - AMO + clauses - 5 variables");
 		while (iterator.isSatisfiable()) {
 			model = iterator.model();
 			assertNotNull(model);
 			System.out.println(new VecInt(model));
 			cpt++;
 		}
+		assertEquals(15, cpt);
+	}
+
+	@Test
+	public void testSimpleCardCaseFor2With7Variables()
+			throws ContradictionException, TimeoutException {
+
+		boolean debug = false;
+
+		int nbVar = 7;
+		solver.newVar(nbVar);
+		IVecInt clause = new VecInt();
+		clause.push(1).push(2).push(3).push(4).push(5).push(6).push(7);
+		IConstr constr1 = solver.addClause(clause);
+		assertNotNull(constr1);
+		IConstr constr2 = solver.addAtMost(clause, 2);
+		assertNotNull(constr2);
+
+		if (debug) {
+			System.out
+					.println("Constraints Simple card case for 2 with 7 variables");
+			for (int i = 0; i < constr2.size(); i++) {
+				System.out.println(((ConstrGroup) constr2).getConstr(i));
+			}
+		}
+
+		ModelIterator iterator = new ModelIterator(solver);
+		int[] model = null;
+		int cpt = 0;
+		System.out
+				.println("testSimpleCardCaseFor2With7Variables models AMO + clause - 7 variables");
+		while (iterator.isSatisfiable()) {
+			model = iterator.model();
+			assertNotNull(model);
+			System.out.println(new VecInt(model));
+			cpt++;
+		}
+		assertEquals(28, cpt);
+	}
+
+	@Test
+	public void testSimpleCardCaseFor2With8Variables()
+			throws ContradictionException, TimeoutException {
+
+		boolean debug = false;
+
+		int nbVar = 8;
+		solver.newVar(nbVar);
+		IVecInt clause = new VecInt();
+		clause.push(1).push(2).push(3).push(4).push(5).push(6).push(7).push(8);
+		IConstr constr1 = solver.addClause(clause);
+		assertNotNull(constr1);
+		IConstr constr2 = solver.addAtMost(clause, 2);
+		assertNotNull(constr2);
+
+		if (debug) {
+			System.out
+					.println("Constraints Simple card case for 2 with 8 variables");
+			for (int i = 0; i < constr2.size(); i++) {
+				System.out.println(((ConstrGroup) constr2).getConstr(i));
+			}
+		}
+
+		ModelIterator iterator = new ModelIterator(solver);
+		int[] model = null;
+		int cpt = 0;
+		System.out
+				.println("testSimpleCardCaseFor2With8Variables models AMO + clause - 8 variables");
+		while (iterator.isSatisfiable()) {
+			model = iterator.model();
+			assertNotNull(model);
+			System.out.println(new VecInt(model));
+			cpt++;
+		}
+		assertEquals(36, cpt);
+	}
+
+	@Test
+	public void testSimpleCardCaseFor4With11Variables()
+			throws ContradictionException, TimeoutException {
+
+		boolean debug = false;
+
+		int nbVar = 11;
+		solver.newVar(nbVar);
+		IVecInt clause = new VecInt();
+		clause.push(1).push(2).push(3).push(4).push(5).push(6).push(7).push(8)
+				.push(9).push(10).push(11);
+		IConstr constr1 = solver.addClause(clause);
+		assertNotNull(constr1);
+		IConstr constr2 = solver.addAtMost(clause, 4);
+		assertNotNull(constr2);
+
+		if (debug) {
+			System.out
+					.println("Constraints Simple card case for 4 with 11 variables");
+			for (int i = 0; i < constr2.size(); i++) {
+				System.out.println(((ConstrGroup) constr2).getConstr(i));
+			}
+		}
+
+		ModelIterator iterator = new ModelIterator(solver);
+		int[] model = null;
+		System.out
+				.println("testSimpleCardCaseFor4With11Variables models AMO + clause - 11 variables");
+		int cpt = 0;
+		while (iterator.isSatisfiable()) {
+			model = iterator.model();
+			assertNotNull(model);
+			System.out.println(new VecInt(model));
+			cpt++;
+		}
+		assertEquals(561, cpt);
 	}
 
 	@Test
@@ -78,5 +345,10 @@ public class TestClausalCardinalities {
 		IConstr constr2 = solver.addAtMost(clause, 0);
 		assertNotNull(constr2);
 		assertFalse(solver.isSatisfiable());
+	}
+
+	@Test
+	public void testName() {
+		System.out.println(solver.toString());
 	}
 }
