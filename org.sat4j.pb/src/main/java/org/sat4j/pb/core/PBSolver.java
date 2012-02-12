bb8263f7fdce24ca86dbefcec5d4713f6ea2274c
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2012-02-12 16:48:24 +0000

    Implemented a clauseDeletionStrategy based on the objective function.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@1176 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

--- org.sat4j.pb/src/main/java/org/sat4j/pb/core/PBSolver.java
+++ org.sat4j.pb/src/main/java/org/sat4j/pb/core/PBSolver.java
@@ -30,7 +30,11 @@ package org.sat4j.pb.core;
 import java.math.BigInteger;
 
 import org.sat4j.core.ConstrGroup;
+import org.sat4j.core.LiteralsUtils;
 import org.sat4j.core.Vec;
+import org.sat4j.minisat.core.ConflictTimer;
+import org.sat4j.minisat.core.ConflictTimerAdapter;
+import org.sat4j.minisat.core.Constr;
 import org.sat4j.minisat.core.IOrder;
 import org.sat4j.minisat.core.LearningStrategy;
 import org.sat4j.minisat.core.RestartStrategy;
@@ -43,6 +47,7 @@ import org.sat4j.specs.ContradictionException;
 import org.sat4j.specs.IConstr;
 import org.sat4j.specs.IVec;
 import org.sat4j.specs.IVecInt;
+import org.sat4j.specs.IteratorInt;
 
 public abstract class PBSolver extends Solver<PBDataStructureFactory> implements
 		IPBSolver {
@@ -152,4 +157,102 @@ public abstract class PBSolver extends Solver<PBDataStructureFactory> implements
 				coeffs, true, weight)));
 		return group;
 	}
+
+	/**
+	 * @since 2.1
+	 */
+	public final LearnedConstraintsDeletionStrategy objectiveFunctionBased = new LearnedConstraintsDeletionStrategy() {
+
+		private static final long serialVersionUID = 1L;
+		private boolean[] inObjectiveFunction;
+
+		private final ConflictTimer clauseManagement = new ConflictTimerAdapter(
+				1000) {
+			private static final long serialVersionUID = 1L;
+			private int nbconflict = 0;
+			private static final int MAX_CLAUSE = 5000;
+			private static final int INC_CLAUSE = 1000;
+			private int nextbound = MAX_CLAUSE;
+
+			@Override
+			public void run() {
+				nbconflict += bound();
+				if (nbconflict >= nextbound) {
+					nextbound += INC_CLAUSE;
+					nbconflict = 0;
+					setNeedToReduceDB(true);
+				}
+			}
+
+			@Override
+			public void reset() {
+				super.reset();
+				nextbound = MAX_CLAUSE;
+				if (nbconflict >= nextbound) {
+					nbconflict = 0;
+					setNeedToReduceDB(true);
+				}
+			}
+		};
+
+		public void reduce(IVec<Constr> learnedConstrs) {
+			int i, j;
+			for (i = j = 0; i < learnedConstrs.size(); i++) {
+				Constr c = learnedConstrs.get(i);
+				if (c.locked() || c.getActivity() <= 2.0) {
+					learnedConstrs.set(j++, learnts.get(i));
+				} else {
+					c.remove(PBSolver.this);
+				}
+			}
+			if (isVerbose()) {
+				System.out
+						.println(getLogPrefix()
+								+ "cleaning " + (learnedConstrs.size() - j) //$NON-NLS-1$
+								+ " clauses out of " + learnedConstrs.size() + "/" + stats.conflicts); //$NON-NLS-1$ //$NON-NLS-2$
+				System.out.flush();
+			}
+			learnts.shrinkTo(j);
+
+		}
+
+		public ConflictTimer getTimer() {
+			return clauseManagement;
+		}
+
+		@Override
+		public String toString() {
+			return "Objective function driven learned constraints deletion strategy";
+		}
+
+		public void init() {
+			inObjectiveFunction = new boolean[nVars()];
+			if (objf == null) {
+				throw new IllegalStateException(
+						"The strategy does not make sense if there is no objective function");
+			}
+			for (IteratorInt it = objf.getVars().iterator(); it.hasNext();) {
+				inObjectiveFunction[Math.abs(it.next())] = true;
+			}
+			clauseManagement.reset();
+		}
+
+		public void onConflict(Constr constr) {
+			boolean fullObj = true;
+
+			for (int i = 0; i < constr.size(); i++) {
+				fullObj = fullObj
+						&& inObjectiveFunction[LiteralsUtils.var(constr.get(i))];
+			}
+			if (fullObj) {
+				constr.incActivity(1.0);
+			} else {
+				constr.incActivity(constr.size());
+			}
+		}
+
+		public void onConflictAnalysis(Constr reason) {
+			// do nothing
+		}
+	};
 }
