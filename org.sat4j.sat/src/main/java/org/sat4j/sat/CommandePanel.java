2ab13496294b2b1c6f83971abddd7011e8513123
sroussel <sroussel@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2012-02-14 16:24:43 +0000

    Added comments at beginning of classes
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@1186 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

--- org.sat4j.sat/src/main/java/org/sat4j/sat/CommandePanel.java
+++ org.sat4j.sat/src/main/java/org/sat4j/sat/CommandePanel.java
@@ -1,3 +1,30 @@
+/*******************************************************************************
+ * SAT4J: a SATisfiability library for Java Copyright (C) 2004-2008 Daniel Le Berre
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU Lesser General Public License Version 2.1 or later (the
+ * "LGPL"), in which case the provisions of the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of the LGPL, and not to allow others to use your version of
+ * this file under the terms of the EPL, indicate your decision by deleting
+ * the provisions above and replace them with the notice and other provisions
+ * required by the LGPL. If you do not delete the provisions above, a recipient
+ * may use your version of this file under the terms of the EPL or the LGPL.
+ * 
+ * Based on the original MiniSat specification from:
+ * 
+ * An extensible SAT solver. Niklas Een and Niklas Sorensson. Proceedings of the
+ * Sixth International Conference on Theory and Applications of Satisfiability
+ * Testing, LNCS 2919, pp 502-518, 2003.
+ *
+ * See www.minisat.se for the original solver in C++.
+ * 
+ *******************************************************************************/
 package org.sat4j.sat;
 
 import java.awt.Dimension;
@@ -10,8 +37,19 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
+/**
+ * 
+ * This panel contains buttons that control restart and clean on solver.
+ * It also displays history of commands.
+ * 
+ * @author sroussel
+ *
+ */
 public class CommandePanel extends JPanel{
 	
+	
+	private static final long serialVersionUID = 1L;
+
 	private TelecommandeStrategy telecomStrategy;
 	
 	public final static String RESTART = "Restart";
--- org.sat4j.sat/src/main/java/org/sat4j/sat/LanceTelecommande_Simple.java
+++ org.sat4j.sat/src/main/java/org/sat4j/sat/LanceTelecommande_Simple.java
@@ -1,3 +1,30 @@
+/*******************************************************************************
+ * SAT4J: a SATisfiability library for Java Copyright (C) 2004-2008 Daniel Le Berre
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU Lesser General Public License Version 2.1 or later (the
+ * "LGPL"), in which case the provisions of the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of the LGPL, and not to allow others to use your version of
+ * this file under the terms of the EPL, indicate your decision by deleting
+ * the provisions above and replace them with the notice and other provisions
+ * required by the LGPL. If you do not delete the provisions above, a recipient
+ * may use your version of this file under the terms of the EPL or the LGPL.
+ * 
+ * Based on the original MiniSat specification from:
+ * 
+ * An extensible SAT solver. Niklas Een and Niklas Sorensson. Proceedings of the
+ * Sixth International Conference on Theory and Applications of Satisfiability
+ * Testing, LNCS 2919, pp 502-518, 2003.
+ *
+ * See www.minisat.se for the original solver in C++.
+ * 
+ *******************************************************************************/
 package org.sat4j.sat;
 
 import java.io.FileNotFoundException;
@@ -18,6 +45,13 @@ import org.sat4j.tools.DecisionTracing;
 import org.sat4j.tools.LearnedClausesSizeTracing;
 import org.sat4j.tools.MultiTracing;
 
+/**
+ * Launches the solver with the remote controller. This controller allows the user to restart the solver and to clean clauses.
+ * 
+ * 
+ * @author sroussel
+ *
+ */
 public class LanceTelecommande_Simple {
 
 	/**
