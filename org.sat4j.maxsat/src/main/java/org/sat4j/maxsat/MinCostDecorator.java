ecb246ec712699d03f1e11f0943f022801761489
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2012-02-22 16:36:26 +0000

    Fixed copyright headers: copyright owner is now Artois University and CNRS.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@1223 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

--- org.sat4j.maxsat/src/main/java/org/sat4j/maxsat/MinCostDecorator.java
+++ org.sat4j.maxsat/src/main/java/org/sat4j/maxsat/MinCostDecorator.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
- * SAT4J: a SATisfiability library for Java Copyright (C) 2004-2008 Daniel Le Berre
+ * SAT4J: a SATisfiability library for Java Copyright (C) 2004, 2012 Artois University and CNRS
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ *  http://www.eclipse.org/legal/epl-v10.html
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU Lesser General Public License Version 2.1 or later (the
@@ -15,6 +15,17 @@
  * the provisions above and replace them with the notice and other provisions
  * required by the LGPL. If you do not delete the provisions above, a recipient
  * may use your version of this file under the terms of the EPL or the LGPL.
+ *
+ * Based on the original MiniSat specification from:
+ *
+ * An extensible SAT solver. Niklas Een and Niklas Sorensson. Proceedings of the
+ * Sixth International Conference on Theory and Applications of Satisfiability
+ * Testing, LNCS 2919, pp 502-518, 2003.
+ *
+ * See www.minisat.se for the original solver in C++.
+ *
+ * Contributors:
+ *   CRIL - initial API and implementation
  *******************************************************************************/
 package org.sat4j.maxsat;
 
