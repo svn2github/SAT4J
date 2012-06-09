3fa9a11a39e068b9daffa31cb0b3fefeae3a20be
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2012-06-09 14:20:42 +0000

    basic implementation of a circular buffer for integers maintaining the average of its elements.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@1369 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

---
+++ org.sat4j.core/src/main/java/org/sat4j/minisat/core/CircularBuffer.java
@@ -0,0 +1,89 @@
+/*******************************************************************************
+ * SAT4J: a SATisfiability library for Java Copyright (C) 2004, 2012 Artois University and CNRS
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU Lesser General Public License Version 2.1 or later (the
+ * "LGPL"), in which case the provisions of the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of the LGPL, and not to allow others to use your version of
+ * this file under the terms of the EPL, indicate your decision by deleting
+ * the provisions above and replace them with the notice and other provisions
+ * required by the LGPL. If you do not delete the provisions above, a recipient
+ * may use your version of this file under the terms of the EPL or the LGPL.
+ *
+ * Based on the original MiniSat specification from:
+ *
+ * An extensible SAT solver. Niklas Een and Niklas Sorensson. Proceedings of the
+ * Sixth International Conference on Theory and Applications of Satisfiability
+ * Testing, LNCS 2919, pp 502-518, 2003.
+ *
+ * See www.minisat.se for the original solver in C++.
+ *
+ * Contributors:
+ *   CRIL - initial API and implementation
+ *******************************************************************************/
+package org.sat4j.minisat.core;
+
+/**
+ * Create a circular buffer of a given capacity allowing to compute efficiently
+ * the mean of the values storied.
+ * 
+ * @author leberre
+ * 
+ */
+public class CircularBuffer {
+
+	private final int[] values;
+	private int index = 0;
+	private long sum = 0;
+	private boolean full = false;
+
+	public CircularBuffer(int capacity) {
+		values = new int[capacity];
+	}
+
+	public void push(int value) {
+		if (!full) {
+			values[index++] = value;
+			sum += value;
+			if (index == values.length) {
+				full = true;
+				index = -1;
+			}
+			return;
+		}
+		index++;
+		if (index == values.length) {
+			index = 0;
+		}
+		// buffer full, overwrite
+		sum -= values[index];
+		values[index] = value;
+		sum += value;
+	}
+
+	public long average() {
+		if (full) {
+			return sum / values.length;
+		}
+		if (index == 0) {
+			return 0;
+		}
+		return sum / index;
+	}
+
+	public void clear() {
+		index = 0;
+		full = false;
+		sum = 0;
+	}
+
+	public boolean isFull() {
+		return full;
+	}
+}
