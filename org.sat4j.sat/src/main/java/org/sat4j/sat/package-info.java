5215d2ecd727e3014a477550fd48cab169a5fd50
sroussel <sroussel@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2012-07-06 08:22:57 +0000

    Replaced package.html files with package-info.java classes in org.sat4j.sat
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@1432 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

---
+++ org.sat4j.sat/src/main/java/org/sat4j/sat/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Implementation of a sat4j Launcher. 
+ * 
+ * Implementation of a remote control for the solver.
+ */
+
+package org.sat4j.sat;
+
---
+++ org.sat4j.sat/src/main/java/org/sat4j/sat/visu/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Implementation of different visualization for the remote control.
+ */
+
+package org.sat4j.sat.visu;
+
