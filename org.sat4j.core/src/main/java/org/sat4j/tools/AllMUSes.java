9858b9e852905232bffc3c6466e403f835ab14b1
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2012-10-02 07:26:15 +0000

    Renamed some internal variables.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@1544 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

--- org.sat4j.core/src/main/java/org/sat4j/tools/AllMUSes.java
+++ org.sat4j.core/src/main/java/org/sat4j/tools/AllMUSes.java
@@ -30,7 +30,6 @@
 package org.sat4j.tools;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.sat4j.core.VecInt;
@@ -75,10 +74,10 @@ public class AllMUSes {
         int nVar = solver.nVars();
 
         if (!(solver instanceof FullClauseSelectorSolver<?>)) {
-            return muses;
-        } else {
-            fcss = (FullClauseSelectorSolver<ISolver>) solver;
+            throw new IllegalArgumentException(
+                    "Requires the solver to be an instance of FullClauseSelectorSolver");
         }
+        fcss = (FullClauseSelectorSolver<ISolver>) solver;
 
         IVecInt pLits = new VecInt();
         for (Integer i : fcss.getAddedVars()) {
@@ -87,7 +86,7 @@ public class AllMUSes {
 
         Minimal4InclusionModel min4Inc = new Minimal4InclusionModel(fcss, pLits);
 
-        IVecInt hardClause;
+        IVecInt blockingClause;
 
         List<IVecInt> secondPhase = new ArrayList<IVecInt>();
         IVecInt secondPhaseClause;
@@ -98,23 +97,20 @@ public class AllMUSes {
         try {
 
             while (min4Inc.isSatisfiable()) {
-                // int[] model = min4Inc.model();
-
                 int[] fullmodel = min4Inc.modelWithInternalVariables();
 
-                hardClause = new VecInt();
+                blockingClause = new VecInt();
                 secondPhaseClause = new VecInt();
-
                 for (int i = 0; i < pLits.size(); i++) {
                     clause = Math.abs(pLits.get(i));
                     if (fullmodel[clause - 1] > 0) {
-                        hardClause.push(-clause);
+                        blockingClause.push(-clause);
                         secondPhaseClause.push(clause);
                     }
                 }
 
                 secondPhase.add(secondPhaseClause);
-                fcss.addNonControlableClause(hardClause);
+                fcss.addBlockingClause(blockingClause);
             }
 
         } catch (TimeoutException e) {
@@ -138,25 +134,21 @@ public class AllMUSes {
                     solver2, pLits);
 
             while (solver2Phase.isSatisfiable()) {
-                hardClause = new VecInt();
+                blockingClause = new VecInt();
                 mus = new VecInt();
 
                 int[] model = solver2Phase.model();
 
-                System.out.println(Arrays.toString(model));
-
                 for (int i = 0; i < model.length; i++) {
                     if (model[i] > 0) {
-                        hardClause.push(-model[i]);
+                        blockingClause.push(-model[i]);
                         mus.push(model[i] - nVar);
                     }
                 }
 
                 muses.add(mus);
 
-                System.out.println(hardClause);
-
-                solver2Phase.addClause(hardClause);
+                solver2Phase.addBlockingClause(blockingClause);
 
             }
 
@@ -170,126 +162,4 @@ public class AllMUSes {
 
         return muses;
     }
-
-    // /**
-    // * Gets an instance of ISolver that can be used to compute all MUSes
-    // * @return the instance of ISolver to which the clauses will be added
-    // */
-    // public static ISolver getProblemInstance() {
-    // WeightedMaxSatDecorator wmd = new
-    // WeightedMaxSatDecorator(SolverFactory.newDefault());
-    // wmd.setNoNewVarForUnitSoftClauses(false);
-    // return wmd;
-    // }
-    //
-    // /**
-    // * Computes all the MUSes associated to the set of constraints added to
-    // the solver
-    // * @param solver the <code>ISolver</code> that contains the set of clauses
-    // * @return a list containing all the MUSes
-    // */
-    // public static List<IVecInt> computeAllMUSes(ISolver solver) {
-    // WeightedMaxSatDecorator wmd;
-    // if(!(solver instanceof WeightedMaxSatDecorator)){
-    // return new ArrayList<IVecInt>();
-    // }
-    // else {
-    // wmd = (WeightedMaxSatDecorator)solver;
-    // }
-    //
-    // int nVar = wmd.nVars();
-    // int realVars = wmd.realNumberOfVariables();
-    //
-    // IPBSolver optimizer = new OptToPBSATAdapter(new PseudoOptDecorator(wmd));
-    //
-    // IVecInt hardClause;
-    //
-    // List<IVecInt> secondPhase = new ArrayList<IVecInt>();
-    // IVecInt secondPhaseClause;
-    //
-    // int clause;
-    //
-    // // first phase
-    // try {
-    // while (optimizer.isSatisfiable()) {
-    // int[] model = optimizer.model();
-    // IVecInt softClauses = wmd.getObjectiveFunction().getVars();
-    //
-    // hardClause = new VecInt();
-    // secondPhaseClause = new VecInt();
-    //
-    // for (int i = 0; i < softClauses.size(); i++) {
-    // clause = Math.abs(softClauses.get(i));
-    // if (optimizer.modelWithInternalVariables()[clause - 1] > 0) {
-    // hardClause.push(-clause);
-    // secondPhaseClause.push(clause);
-    // }
-    // }
-    //
-    // secondPhase.add(secondPhaseClause);
-    // wmd.addHardClause(hardClause);
-    // }
-    //
-    // } catch (TimeoutException e) {
-    // e.printStackTrace();
-    // } catch (ContradictionException e) {
-    //
-    // }
-    //
-    // // System.out.println("MSS = " + secondPhase.size());
-    //
-    //
-    // // second phase
-    // List<IVecInt> muses = new ArrayList<IVecInt>();
-    // IVecInt mus;
-    // try {
-    // IPBSolver pbs = org.sat4j.pb.SolverFactory.newDefault();
-    //
-    // int pbVar = realVars - nVar;
-    // pbs.newVar(realVars);
-    //
-    // for (IVecInt c : secondPhase) {
-    // pbs.addClause(c);
-    // }
-    //
-    // int[] objInt = new int[pbVar];
-    // BigInteger[] objCoeffs = new BigInteger[pbVar];
-    //
-    // for (int i = 0; i < pbVar; i++) {
-    // objInt[i] = i + nVar + 1;
-    // objCoeffs[i] = BigInteger.ONE;
-    // }
-    //
-    // ObjectiveFunction obj = new ObjectiveFunction(new VecInt(objInt),
-    // new Vec<BigInteger>(objCoeffs));
-    // pbs.setObjectiveFunction(obj);
-    //
-    // IPBSolver opt2 = new OptToPBSATAdapter(new PseudoOptDecorator(pbs));
-    // while (opt2.isSatisfiable()) {
-    // hardClause = new VecInt();
-    // mus = new VecInt();
-    //
-    // for (int i = 0; i < opt2.model().length; i++) {
-    // if (opt2.model()[i] > 0) {
-    // hardClause.push(-opt2.model()[i]);
-    // mus.push(opt2.model()[i] - nVar);
-    // }
-    // }
-    //
-    // muses.add(mus);
-    //
-    // opt2.addClause(hardClause);
-    //
-    //
-    // }
-    //
-    // } catch (ContradictionException e) {
-    //
-    // } catch (TimeoutException e) {
-    // e.printStackTrace();
-    // }
-    //
-    // return muses;
-    // }
-
 }
