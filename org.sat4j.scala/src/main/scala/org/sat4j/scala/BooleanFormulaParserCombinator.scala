d2b6bafa947c0f9b0ef9d046f7b3013f6289de7a
sroussel <sroussel@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2012-12-04 15:12:54 +0000

    First draft for a parser for boolean formula : allows to transform a string to a list of BoolExp
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@1653 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

---
+++ org.sat4j.scala/src/main/scala/org/sat4j/scala/BooleanFormulaParserCombinator.scala
@@ -0,0 +1,61 @@
+package org.sat4j.scala
+
+import scala.collection.immutable.List.apply
+import scala.util.parsing.combinator.PackratParsers
+import scala.util.parsing.combinator.syntactical.StandardTokenParsers
+
+import Logic._
+
+object BooleanFormulaParserCombinator extends StandardTokenParsers with PackratParsers {
+
+
+  lexical.delimiters += ("(", ")", "&", "|", "~", "->", "<->", ";")
+    
+  def scala2JavaList(sl: List[String]): java.util.List[String] = {
+    var jl = new java.util.ArrayList[String]()
+    sl.foreach(jl.add(_))
+    jl
+  }
+  
+  val listFormula: PackratParser[List[BoolExp]] =  
+    (formula <~ ";") ~ listFormula ^^ {
+      case f ~ l => f :: l
+    } | formula <~ ";" ^^ {
+    case f => f :: List()
+    } 
+
+  val formula: PackratParser[BoolExp] =  term ~ ("&" ~> formula) ^^ {
+    case f1 ~ f2 => And(f1, f2)
+  } | term ~ ("|" ~> formula) ^^ {
+    case f1 ~ f2 => Or(f1, f2)
+  } | term ~ ("->" ~> formula) ^^ {
+    case f1 ~ f2 => Implies(f1, f2)
+  } | term ~ ("<->" ~> formula) ^^ {
+    case f1 ~ f2 => Iff(f1, f2)
+  } | term
+  
+  val term = "(" ~> formula <~ ")" | "~" ~> "(" ~> formula <~ ")" ^^ {
+    case f  => Not(f)
+  } | lit
+
+  val lit: PackratParser[BoolExp] =  "~" ~> ident ^^ {
+    case s => Not(Ident(s.toString()))
+  } | ident ^^ {
+    case s => Ident(s.toString())
+  } 
+  
+  def parseListBoolExp(dsl : String) = 
+    listFormula(new PackratReader(new lexical.Scanner(dsl))) match {
+    case Success(ord, _) => ord map(f => (PrettyPrint(f), f toCnfList))
+    case Failure(msg, _) => msg :: List()
+    case Error(msg, _) => msg :: List()
+    case p => p.toString() :: List() 
+  }
+  
+  def javaParseListBool(dsl: String) = println(parseListBoolExp(dsl))
+
+  def main(args: Array[String]) = {
+    println(parseListBoolExp("(x & y) <-> z; a | b; ~a & b | c;"))
+    javaParseListBool("a & b & c;")
+  }
+}
