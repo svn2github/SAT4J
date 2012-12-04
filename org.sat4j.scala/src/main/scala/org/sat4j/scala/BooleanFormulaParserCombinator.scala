8e4062ae7a5a0cc4e2356b18b1d7f4e520198c8b
sroussel <sroussel@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2012-12-04 15:18:10 +0000

    Added a few comments to BooleanFormulaParser
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@1654 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

--- org.sat4j.scala/src/main/scala/org/sat4j/scala/BooleanFormulaParserCombinator.scala
+++ org.sat4j.scala/src/main/scala/org/sat4j/scala/BooleanFormulaParserCombinator.scala
@@ -8,7 +8,14 @@ import Logic._
 
 object BooleanFormulaParserCombinator extends StandardTokenParsers with PackratParsers {
 
-
+  /**
+   * listFormula = formula ; | formula ; listFormula
+   * formula = term & formula | term '|' formula | term -> formula | term <-> formula | term
+   * term = (formula) | ~(formula) | lit
+   * lit = ~ ident | ident
+   */
+  
+  
   lexical.delimiters += ("(", ")", "&", "|", "~", "->", "<->", ";")
     
   def scala2JavaList(sl: List[String]): java.util.List[String] = {
@@ -34,9 +41,9 @@ object BooleanFormulaParserCombinator extends StandardTokenParsers with PackratP
     case f1 ~ f2 => Iff(f1, f2)
   } | term
   
-  val term = "(" ~> formula <~ ")" | "~" ~> "(" ~> formula <~ ")" ^^ {
+  val term =  "~" ~> "(" ~> formula <~ ")" ^^ {
     case f  => Not(f)
-  } | lit
+  } | "(" ~> formula <~ ")" | lit
 
   val lit: PackratParser[BoolExp] =  "~" ~> ident ^^ {
     case s => Not(Ident(s.toString()))
@@ -44,6 +51,10 @@ object BooleanFormulaParserCombinator extends StandardTokenParsers with PackratP
     case s => Ident(s.toString())
   } 
   
+  /**
+   * Parses a string into a couple (s,l) where s is a string representing the BoolExp with a PrettyPrint 
+   * and l the list of Cnf composing the BoolExp
+   */
   def parseListBoolExp(dsl : String) = 
     listFormula(new PackratReader(new lexical.Scanner(dsl))) match {
     case Success(ord, _) => ord map(f => (PrettyPrint(f), f toCnfList))
@@ -55,7 +66,7 @@ object BooleanFormulaParserCombinator extends StandardTokenParsers with PackratP
   def javaParseListBool(dsl: String) = println(parseListBoolExp(dsl))
 
   def main(args: Array[String]) = {
-    println(parseListBoolExp("(x & y) <-> z; a | b; ~a & b | c;"))
-    javaParseListBool("a & b & c;")
+    println(parseListBoolExp("(x & y) <-> z; a | b; ~a & b | c; ~(a & b)"))
+    javaParseListBool("~(a & b);")
   }
 }
