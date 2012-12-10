574c03e4ad31f4f38d77084bf752cce4fa46ef73
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2012-12-10 09:27:55 +0000

    Test cases showing the encoding of PB constraints.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@1664 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

---
+++ org.sat4j.pb/src/main/java/org/sat4j/pb/reader/JSONPBReader.java
@@ -0,0 +1,74 @@
+package org.sat4j.pb.reader;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.sat4j.core.VecInt;
+import org.sat4j.pb.IPBSolver;
+import org.sat4j.reader.JSONReader;
+import org.sat4j.reader.ParseFormatException;
+import org.sat4j.specs.ContradictionException;
+import org.sat4j.specs.IVecInt;
+
+public class JSONPBReader extends JSONReader<IPBSolver> {
+    public static final String WLITERAL = "\\[(-?\\d+),(-?\\d+)\\]";
+    public static final String WCLAUSE = "(\\[(" + WLITERAL + "(," + WLITERAL
+            + ")*)?\\])";
+    public static final String PB = "(\\[" + WCLAUSE + ",'[=<>]=?',-?\\d+\\])";
+    public static final Pattern pseudo = Pattern.compile(PB);
+    public static final Pattern wclause = Pattern.compile(WCLAUSE);
+    public static final Pattern wliteral = Pattern.compile(WLITERAL);
+
+    public JSONPBReader(IPBSolver solver) {
+        super(solver);
+    }
+
+    @Override
+    protected void handleNotHandled(String constraint)
+            throws ParseFormatException, ContradictionException {
+        if (pseudo.matcher(constraint).matches()) {
+            handlePB(constraint);
+        } else {
+            throw new UnsupportedOperationException("Wrong formula "
+                    + constraint);
+        }
+    }
+
+    private void handlePB(String constraint) throws ContradictionException {
+        Matcher matcher = wclause.matcher(constraint);
+        if (matcher.find()) {
+            String wclause = matcher.group();
+            constraint = matcher.replaceFirst("");
+            matcher = wliteral.matcher(wclause);
+            IVecInt literals = new VecInt();
+            IVecInt coefs = new VecInt();
+            while (matcher.find()) {
+                literals.push(Integer.valueOf(matcher.group(2)));
+                coefs.push(Integer.valueOf(matcher.group(1)));
+            }
+            String[] pieces = constraint.split(",");
+            String comp = pieces[1].substring(1, pieces[1].length() - 1);
+            int degree = Integer.valueOf(pieces[2].substring(0,
+                    pieces[2].length() - 1));
+            if ("=".equals(comp) || "==".equals(comp)) {
+                solver.addExactly(literals, coefs, degree);
+            } else if ("<=".equals(comp)) {
+                solver.addAtMost(literals, coefs, degree);
+            } else if ("<".equals(comp)) {
+                solver.addAtMost(literals, coefs, degree - 1);
+            } else if (">=".equals(comp)) {
+                solver.addAtLeast(literals, coefs, degree);
+            } else {
+                assert ">".equals(comp);
+                solver.addAtLeast(literals, coefs, degree + 1);
+            }
+        }
+
+    }
+
+    @Override
+    protected String constraintPattern() {
+        return "(" + CLAUSE + "|" + CARD + "|" + PB + ")";
+    }
+
+}
