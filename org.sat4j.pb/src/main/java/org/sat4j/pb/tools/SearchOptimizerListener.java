b51ea327204d335fadd01d9ccd37937430c9d062
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2013-01-16 13:05:10 +0000

    Displaying expected o line in case of optimization (to work well with the eval framework).
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@1742 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

--- org.sat4j.pb/src/main/java/org/sat4j/pb/tools/SearchOptimizerListener.java
+++ org.sat4j.pb/src/main/java/org/sat4j/pb/tools/SearchOptimizerListener.java
@@ -1,16 +1,45 @@
+/*******************************************************************************
+ * SAT4J: a SATisfiability library for Java Copyright (C) 2004, 2012 Artois University and CNRS
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU Lesser General Public License Version 2.1 or later (the
+ * "LGPL"), in which case the provisions of the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of the LGPL, and not to allow others to use your version of
+ * this file under the terms of the EPL, indicate your decision by deleting
+ * the provisions above and replace them with the notice and other provisions
+ * required by the LGPL. If you do not delete the provisions above, a recipient
+ * may use your version of this file under the terms of the EPL or the LGPL.
+ *
+ * Based on the original MiniSat specification from:
+ *
+ * An extensible SAT solver. Niklas Een and Niklas Sorensson. Proceedings of the
+ * Sixth International Conference on Theory and Applications of Satisfiability
+ * Testing, LNCS 2919, pp 502-518, 2003.
+ *
+ * See www.minisat.se for the original solver in C++.
+ *
+ * Contributors:
+ *   CRIL - initial API and implementation
+ *******************************************************************************/
 package org.sat4j.pb.tools;
 
 import java.math.BigInteger;
 
+import org.sat4j.ILauncherMode;
 import org.sat4j.pb.IPBSolverService;
 import org.sat4j.pb.ObjectiveFunction;
-import org.sat4j.specs.IConstr;
 import org.sat4j.specs.Lbool;
-import org.sat4j.specs.SearchListener;
+import org.sat4j.tools.SearchListenerAdapter;
 import org.sat4j.tools.SolutionFoundListener;
 
-public class SearchOptimizerListener implements
-        SearchListener<IPBSolverService> {
+public class SearchOptimizerListener extends
+        SearchListenerAdapter<IPBSolverService> {
 
     /**
 	 * 
@@ -27,88 +56,32 @@ public class SearchOptimizerListener implements
         this.sfl = sfl;
     }
 
+    @Override
     public void init(IPBSolverService solverService) {
         this.obj = solverService.getObjectiveFunction();
         this.solverService = solverService;
     }
 
-    public void assuming(int p) {
-        // TODO Auto-generated method stub
-
-    }
-
-    public void propagating(int p, IConstr reason) {
-        // TODO Auto-generated method stub
-
-    }
-
-    public void backtracking(int p) {
-        // TODO Auto-generated method stub
-
-    }
-
-    public void adding(int p) {
-        // TODO Auto-generated method stub
-
-    }
-
-    public void learn(IConstr c) {
-        // TODO Auto-generated method stub
-
-    }
-
-    public void delete(int[] clause) {
-        // TODO Auto-generated method stub
-
-    }
-
-    public void conflictFound(IConstr confl, int dlevel, int trailLevel) {
-        // TODO Auto-generated method stub
-
-    }
-
-    public void conflictFound(int p) {
-        // TODO Auto-generated method stub
-
-    }
-
+    @Override
     public void solutionFound(int[] model) {
         if (obj != null) {
             BigInteger modelDegree = obj.calculateDegree(model);
-            System.out.println("c objective function value = " + modelDegree);
+            System.out.println(ILauncherMode.CURRENT_OPTIMUM_VALUE_PREFIX
+                    + modelDegree);
             this.solverService.addAtMostOnTheFly(obj.getVars(),
                     obj.getCoeffs(), modelDegree.subtract(BigInteger.ONE));
         }
         sfl.onSolutionFound(model);
     }
 
-    public void beginLoop() {
-        // TODO Auto-generated method stub
-
-    }
-
-    public void start() {
-        // TODO Auto-generated method stub
-
-    }
-
+    @Override
     public void end(Lbool result) {
         if (result == Lbool.FALSE)
             sfl.onUnsatTermination();
     }
 
-    public void restarting() {
-        // TODO Auto-generated method stub
-
-    }
-
-    public void backjump(int backjumpLevel) {
-        // TODO Auto-generated method stub
-
-    }
-
-    public void cleaning() {
-        // TODO Auto-generated method stub
-
+    @Override
+    public String toString() {
+        return "Internal optimizer search listener";
     }
 }
