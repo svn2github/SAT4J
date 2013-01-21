2e8af1c5f82c65a4e6431098dba687d03f26142e
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2013-01-21 19:29:51 +0000

    Added copyright header and javadoc.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@1823 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

--- org.sat4j.core/src/main/java/org/sat4j/reader/JSONReader.java
+++ org.sat4j.core/src/main/java/org/sat4j/reader/JSONReader.java
@@ -1,3 +1,32 @@
+/*******************************************************************************
+ * SAT4J: a SATisfiability library for Java Copyright (C) 2004, 2012 Artois University and CNRS
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU Lesser General Public License Version 2.1 or later (the
+ * "LGPL"), in which case the provisions of the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of the LGPL, and not to allow others to use your version of
+ * this file under the terms of the EPL, indicate your decision by deleting
+ * the provisions above and replace them with the notice and other provisions
+ * required by the LGPL. If you do not delete the provisions above, a recipient
+ * may use your version of this file under the terms of the EPL or the LGPL.
+ *
+ * Based on the original MiniSat specification from:
+ *
+ * An extensible SAT solver. Niklas Een and Niklas Sorensson. Proceedings of the
+ * Sixth International Conference on Theory and Applications of Satisfiability
+ * Testing, LNCS 2919, pp 502-518, 2003.
+ *
+ * See www.minisat.se for the original solver in C++.
+ *
+ * Contributors:
+ *   CRIL - initial API and implementation
+ *******************************************************************************/
 package org.sat4j.reader;
 
 import java.io.BufferedReader;
@@ -15,6 +44,21 @@ import org.sat4j.specs.IProblem;
 import org.sat4j.specs.ISolver;
 import org.sat4j.specs.IVecInt;
 
+/**
+ * Simple JSON reader for clauses and cardinality constraints.
+ * 
+ * Clauses are represented as an array of Dimacs literals (non zero integers).
+ * Cardinality constraints are represented like a clause for its left hand side,
+ * a comparator (a string) and a number.
+ * <code>[[-1,-2,-3],[[1,-2,3],'>',2],[4,-3,6]]</code> for instance represents
+ * three constraints, two clauses and the cardinality constraint x1 + not x2 +
+ * x3 > 2.
+ * 
+ * @author leberre
+ * 
+ * @param <S>
+ *            the type of solver to feed.
+ */
 public class JSONReader<S extends ISolver> extends Reader {
 
     protected final S solver;
