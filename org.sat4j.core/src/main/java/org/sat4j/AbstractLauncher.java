c3bdffd6cb953467b1b8110661084dfbab39665b
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2013-05-26 20:49:32 +0000

    Add a new method to allow launchers to get access to launcher mode.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@2050 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

--- org.sat4j.core/src/main/java/org/sat4j/AbstractLauncher.java
+++ org.sat4j.core/src/main/java/org/sat4j/AbstractLauncher.java
@@ -100,6 +100,10 @@ public abstract class AbstractLauncher implements Serializable, ILogAble {
         this.launcherMode = launcherMode;
     }
 
+    protected ILauncherMode getLauncherMode() {
+        return this.launcherMode;
+    }
+
     protected void setIncomplete(boolean isIncomplete) {
         this.launcherMode.setIncomplete(isIncomplete);
     }
