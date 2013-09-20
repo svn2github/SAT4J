674bb799085d89a1b6cb7955076239bb6150d2fa
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2013-09-20 07:47:50 +0000

    Fix for test case BugSAT32.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@2105 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

--- org.sat4j.pb/src/main/java/org/sat4j/pb/constraints/pb/Pseudos.java
+++ org.sat4j.pb/src/main/java/org/sat4j/pb/constraints/pb/Pseudos.java
@@ -99,9 +99,14 @@ public abstract class Pseudos {
             IVec<BigInteger> bigCoefs, boolean moreThan, BigInteger bigDeg,
             ILits voc) throws ContradictionException {
         // Ajouter les simplifications quand la structure sera d?finitive
-        if (ps.size() == 0 && bigDeg.signum() > 0) {
-            throw new ContradictionException("Creating Empty clause ?");
-        } else if (ps.size() != bigCoefs.size()) {
+        if (ps.size() == 0) {
+            if (bigDeg.signum() > 0) {
+                throw new ContradictionException("Creating Empty clause ?");
+            }
+            // ignoring tautological constraint
+            return null;
+        }
+        if (ps.size() != bigCoefs.size()) {
             throw new IllegalArgumentException(
                     "Contradiction dans la taille des tableaux ps=" + ps.size()
                             + " coefs=" + bigCoefs.size() + ".");
@@ -113,9 +118,14 @@ public abstract class Pseudos {
             BigInteger[] bigCoefs, boolean moreThan, BigInteger bigDeg)
             throws ContradictionException {
         // Ajouter les simplifications quand la structure sera d?finitive
-        if (ps.length == 0 && bigDeg.signum() > 0) {
-            throw new ContradictionException("Creating Empty clause ?");
-        } else if (ps.length != bigCoefs.length) {
+        if (ps.length == 0) {
+            if (bigDeg.signum() > 0) {
+                throw new ContradictionException("Creating Empty clause ?");
+            }
+            // ignoring tautological constraint
+            return null;
+        }
+        if (ps.length != bigCoefs.length) {
             throw new IllegalArgumentException(
                     "Contradiction dans la taille des tableaux ps=" + ps.length
                             + " coefs=" + bigCoefs.length + ".");
