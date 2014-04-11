a0ee2a862ecdbb2791f3ee215db860732a6db686
sroussel <sroussel@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2014-04-11 12:58:42 +0000

    added a test case for lexico pb
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@2309 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

--- org.sat4j.pb/src/test/java/org/sat4j/pb/LexicoDecoratorPBTest.java
+++ org.sat4j.pb/src/test/java/org/sat4j/pb/LexicoDecoratorPBTest.java
@@ -1,11 +1,14 @@
 package org.sat4j.pb;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.fail;
+
+import java.util.Arrays;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.sat4j.core.VecInt;
-import org.sat4j.pb.SolverFactory;
 import org.sat4j.pb.tools.LexicoDecoratorPB;
 import org.sat4j.specs.ContradictionException;
 import org.sat4j.specs.IVecInt;
@@ -13,14 +16,16 @@ import org.sat4j.specs.TimeoutException;
 
 public class LexicoDecoratorPBTest {
 
+    private LexicoDecoratorPB lexico;
+
     @Before
     public void setUp() throws Exception {
+        lexico = new LexicoDecoratorPB(SolverFactory.newDefault());
     }
 
     @Test
     public void test() throws ContradictionException, TimeoutException {
-        LexicoDecoratorPB lexico = new LexicoDecoratorPB(
-                SolverFactory.newDefault());
+
         IVecInt clause = new VecInt();
         clause.push(-1).push(-2);
         lexico.addClause(clause);
@@ -37,5 +42,36 @@ public class LexicoDecoratorPBTest {
         clause2.push(1).push(2);
         lexico.addCriterion(clause2);
         assertFalse(lexico.admitABetterSolution());
+    }
+
+    @Test
+    public void testSimple() throws ContradictionException, TimeoutException {
+        IVecInt clause = new VecInt();
+        clause.push(1).push(2).push(3);
+        lexico.addClause(clause);
+        clause.clear();
+        clause.push(2).push(4).push(5);
+        lexico.addClause(clause);
+        clause.clear();
+        clause.push(6).push(7).push(8);
+        lexico.addClause(clause);
+        clause.clear();
+        clause.push(1).push(2).push(3).push(4).push(5).push(6).push(7).push(8);
+        lexico.addCriterion(clause);
+
+        try {
+            if (lexico.admitABetterSolution()) {
+                int[] expectedModel = new int[] { -1, 2, -3, -4, -5, 6, -7, -8 };
+                int[] actualModel = lexico.model();
+                for (int i = 0; i < expectedModel.length; ++i) {
+                    assertEquals(expectedModel[i], actualModel[i]);
+                }
+                System.out.println(Arrays.toString(actualModel));
+                System.out.println(lexico.getObjectiveValue());
+
+            }
+        } catch (TimeoutException e) {
+            fail(e.getMessage());
+        }
     }
 }
