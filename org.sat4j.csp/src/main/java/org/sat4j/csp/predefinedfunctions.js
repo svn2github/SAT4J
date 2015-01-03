b0d056226f2328103720341cc11947b91b94cd6f
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2015-01-03 17:43:45 +0000

    Removed tabs. Fixed === vs == comparisons.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@2394 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

--- org.sat4j.csp/src/main/java/org/sat4j/csp/predefinedfunctions.js
+++ org.sat4j.csp/src/main/java/org/sat4j/csp/predefinedfunctions.js
@@ -1,103 +1,103 @@
 function neg(x) {
-	return -x;
+    return -x;
 }
 
 function abs(x) {
-	if (x > 0) {
-		return x;
-	}
-	return -x;
+    if (x > 0) {
+        return x;
+    }
+    return -x;
 }
 
 function add(x, y) {
-	return x + y;
+    return x + y;
 }
 
 function sub(x, y) {
-	return x - y;
+    return x - y;
 }
 
 function mul(x, y) {
-	return x * y;
+    return x * y;
 }
 
 function div(x, y) {
-	return Math.floor(x / y);
+    return Math.floor(x / y);
 }
 
 function mod(x, y) {
-	return x % y;
+    return x % y;
 }
 
 function pow(x, y) {
-	t = x;
-	for (i = 1; i < y; i++) {
-		t = t * x;
-	}
-	return t;
+    t = x;
+    for (i = 1; i < y; i++) {
+        t = t * x;
+    }
+    return t;
 }
 
 function min(x, y) {
-	if (x < y) {
-		return x;
-	}
-	return y;
+    if (x < y) {
+        return x;
+    }
+    return y;
 }
 
 function max(x, y) {
-	if (x < y) {
-		return y;
-	}
-	return x;
+    if (x < y) {
+        return y;
+    }
+    return x;
 }
 
 function not(x) {
-	return !x;
+    return !x;
 }
 
 function and(x, y) {
-	return x && y;
+    return x && y;
 }
 
 function or(x, y) {
-	return x || y;
+    return x || y;
 }
 
 function xor(x, y) {
-	return x && !y || !x && y;
+    return x && !y || !x && y;
 }
 
 function eq(x, y) {
-	return x == y;
+    return x === y;
 }
 
 function ne(x, y) {
-	return x != y;
+    return x !== y;
 }
 
 function lt(x, y) {
-	return x < y;
+    return x < y;
 }
 
 function le(x, y) {
-	return x <= y;
+    return x <= y;
 }
 
 function gt(x, y) {
-	return x > y;
+    return x > y;
 }
 
 function ge(x, y) {
-	return x >= y;
+    return x >= y;
 }
 
 function ite(x, y, z) {
-	if (x) {
-		return y;
-	}
-	return z;
+    if (x) {
+        return y;
+    }
+    return z;
 }
 
 function iff(x, y) {
-	return x && y || !x && !y;
+    return x && y || !x && !y;
 }
