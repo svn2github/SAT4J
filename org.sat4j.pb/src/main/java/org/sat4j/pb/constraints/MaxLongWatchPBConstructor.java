be3cda94215f7499ac79d6a1b2aae65a464eb753
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2015-07-18 17:51:33 +0000

    Fix for failing TestLonca. The same work must be performed on the other PB constructors.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@2419 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

--- org.sat4j.pb/src/main/java/org/sat4j/pb/constraints/MaxLongWatchPBConstructor.java
+++ org.sat4j.pb/src/main/java/org/sat4j/pb/constraints/MaxLongWatchPBConstructor.java
@@ -51,12 +51,18 @@ public class MaxLongWatchPBConstructor implements IPBConstructor {
     public Constr constructPB(UnitPropagationListener solver, ILits voc,
             int[] theLits, BigInteger[] coefs, BigInteger degree,
             BigInteger sumCoefs) throws ContradictionException {
+        Constr constr;
         if (sumCoefs.bitLength() < Long.SIZE) {
-            return MaxWatchPbLong.normalizedMaxWatchPbNew(solver, voc, theLits,
+            constr = MaxWatchPbLong.normalizedMaxWatchPbNew(solver, voc,
+                    theLits, coefs, degree, sumCoefs);
+        } else {
+            constr = MaxWatchPb.normalizedMaxWatchPbNew(solver, voc, theLits,
                     coefs, degree, sumCoefs);
         }
-        return MaxWatchPb.normalizedMaxWatchPbNew(solver, voc, theLits, coefs,
-                degree, sumCoefs);
+        if (constr == null) {
+            return Constr.TAUTOLOGY;
+        }
+        return constr;
     }
 
     // public static boolean isLongSufficient(BigInteger[] coefs, BigInteger
