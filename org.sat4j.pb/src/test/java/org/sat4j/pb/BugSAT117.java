b2770e7a40b63710c6733afb0049eac4857b617e
leberre <leberre@ba638df5-4473-46d1-82f8-c3ae2a17a6e1>
2015-11-01 15:50:59 +0000

    Added another test case just to make sure that the issue is correctly fixed. Takes 60s more for CI.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/sat4j/maven/trunk@2430 ba638df5-4473-46d1-82f8-c3ae2a17a6e1

--- org.sat4j.pb/src/test/java/org/sat4j/pb/BugSAT117.java
+++ org.sat4j.pb/src/test/java/org/sat4j/pb/BugSAT117.java
@@ -5,6 +5,9 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,6 +16,8 @@ import org.sat4j.specs.ContradictionException;
 import org.sat4j.specs.ISolver;
 import org.sat4j.specs.IVecInt;
 import org.sat4j.specs.TimeoutException;
+import org.sat4j.tools.GateTranslator;
+import org.sat4j.tools.ModelIterator;
 
 public class BugSAT117 {
 
@@ -111,5 +116,57 @@ public class BugSAT117 {
             field.setAccessible(true);
             assertNull(field.get(solver.getSolvingEngine()));
         }
+    }
+
+    @Test
+    public void testFromPaul() {
+        // This test will run for 60s, may want to disable it for fast unit
+        // testing.
+        final ISolver solver = SolverFactory.newLight();
+        GateTranslator gateTranslator = new GateTranslator(solver);
+        ModelIterator modelIterator = new ModelIterator(solver);
+
+        Date begin = new Date();
+        long beginTime = begin.getTime();
+        Date now = new Date();
+        while (now.getTime() - beginTime < 60000) {
+            solve(gateTranslator, modelIterator);
+            now = new Date();
+        }
+    }
+
+    private List<List<Integer>> solve(GateTranslator gateTranslator,
+            ModelIterator modelIterator) {
+        List<List<Integer>> solution = new ArrayList<List<Integer>>();
+        solver.newVar(5);
+
+        // (x1 ∨ ¬x5 ∨ x4) ∧ (¬x1 ∨ x5 ∨ x3 ∨ x4)
+        try {
+            gateTranslator.not(6, 5);
+            IVecInt disjuncts1 = new VecInt(new int[] { 1, 6, 4 });
+            gateTranslator.or(7, disjuncts1);
+            gateTranslator.not(8, 1);
+            IVecInt disjuncts2 = new VecInt(new int[] { 8, 5, 3, 4 });
+            gateTranslator.or(9, disjuncts2);
+            gateTranslator.gateTrue(9);
+            while (modelIterator.isSatisfiable()) {
+                final int[] model = modelIterator.model();
+                List<Integer> modelList = new ArrayList<Integer>(model.length);
+                for (int literal : model) {
+                    modelList.add(literal);
+                }
+                solution.add(modelList);
+            }
+        } catch (ContradictionException ex) {
+            ex.printStackTrace();
+        } catch (TimeoutException ex) {
+            ex.printStackTrace();
+        } finally {
+            gateTranslator.reset();
+            solver.reset();
+            modelIterator.reset();
+        }
+
+        return solution;
     }
 }
